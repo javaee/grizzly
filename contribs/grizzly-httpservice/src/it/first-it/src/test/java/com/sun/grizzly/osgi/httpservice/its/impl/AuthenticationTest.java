75e0c7e83a70a15a5a7a47fb35efad375c45496d
neotyk <neotyk@04fdb32c-e91d-0410-84f5-98e489d73e21>
2009-02-18 00:31:45 +0000

    https://grizzly.dev.java.net/issues/show_bug.cgi?id=415 Created Authentication IT, works just fine.
    git-svn-id: https://svn.java.net/svn/grizzly~svn/trunk/code@2488 04fdb32c-e91d-0410-84f5-98e489d73e21

---
+++ contribs/grizzly-httpservice/src/it/first-it/src/test/java/com/sun/grizzly/osgi/httpservice/its/impl/AuthenticationTest.java
@@ -0,0 +1,164 @@
+/*
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 2007-2008 Sun Microsystems, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License. You can obtain
+ * a copy of the License at https://glassfish.dev.java.net/public/CDDL+GPL.html
+ * or glassfish/bootstrap/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at glassfish/bootstrap/legal/LICENSE.txt.
+ * Sun designates this particular file as subject to the "Classpath" exception
+ * as provided by Sun in the GPL Version 2 section of the License file that
+ * accompanied this code.  If applicable, add the following below the License
+ * Header, with the fields enclosed by brackets [] replaced by your own
+ * identifying information: "Portions Copyrighted [year]
+ * [name of copyright owner]"
+ *
+ * Contributor(s):
+ *
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ *
+ */
+package com.sun.grizzly.osgi.httpservice.its.impl;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static org.ops4j.pax.exam.CoreOptions.*;
+import org.ops4j.pax.exam.Option;
+import static org.ops4j.pax.exam.container.def.PaxRunnerOptions.*;
+import org.ops4j.pax.exam.junit.Configuration;
+import org.ops4j.pax.exam.junit.JUnit4TestRunner;
+import org.osgi.framework.BundleContext;
+import org.osgi.service.http.HttpContext;
+import org.osgi.service.http.HttpService;
+import org.osgi.service.http.NamespaceException;
+import org.osgi.util.tracker.ServiceTracker;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.Authenticator;
+import java.net.HttpURLConnection;
+import java.net.PasswordAuthentication;
+import java.net.URL;
+
+/**
+ * Integration testing of Authentication.
+ *
+ * @author Hubert Iwaniuk
+ * @since Feb 9, 2009
+ */
+@RunWith(JUnit4TestRunner.class)
+public class AuthenticationTest {
+
+    @Configuration
+    public static Option[] configuration() {
+        return options(
+                repositories(
+                        repository("http://repository.springsource.com/maven/bundles/external"),
+                        repository("http://repository.ops4j.org/mvn-snapshots").disableReleases().allowSnapshots(),
+                        repository("http://repo1.maven.org/maven2/")
+                ),
+                logProfile(),
+                frameworks(
+                        felix()
+                ),
+                systemProperty("org.osgi.service.http.port").value("8080"),
+                provision(
+                        mavenBundle().groupId("com.sun.grizzly").artifactId("grizzly-http-servlet").version(
+                                "1.9.6-SNAPSHOT"),
+                        mavenBundle().groupId("com.sun.grizzly").artifactId("grizzly-http").version("1.9.6-SNAPSHOT"),
+                        mavenBundle().groupId("com.sun.grizzly").artifactId("grizzly-utils").version("1.9.6-SNAPSHOT"),
+                        mavenBundle().groupId("com.sun.grizzly").artifactId("grizzly-framework").version(
+                                "1.9.6-SNAPSHOT"),
+                        mavenBundle().groupId("com.sun.grizzly").artifactId("grizzly-portunif").version(
+                                "1.9.6-SNAPSHOT"),
+                        mavenBundle().groupId("com.sun.grizzly").artifactId("grizzly-rcm").version("1.9.6-SNAPSHOT"),
+                        mavenBundle().groupId("com.sun.grizzly.osgi").artifactId("grizzly-httpservice").version(
+                                "1.9.6-SNAPSHOT")
+                )
+        );
+    }
+
+    @Test
+    public void authentication(final BundleContext bc)
+            throws InterruptedException, ServletException, NamespaceException, IOException {
+        final ServiceTracker tracker = new ServiceTracker(bc, HttpService.class.getName(), null);
+        tracker.open();
+        tracker.waitForService(1000);
+        HttpService hs = (HttpService) tracker.getService();
+        try {
+            HttpContext context = new HttpContext() {
+                public boolean handleSecurity(HttpServletRequest request, HttpServletResponse response)
+                        throws IOException {
+                    // TODO is it a bug HttpServletRequest.BASIC_AUTH.equals(request.getAuthType()) doesn't provide info if Authorization header present?
+                    String auth = request.getHeader("Authorization");
+                    if (auth != null) {
+                        String userpassEncoded = auth.substring(6);
+                        sun.misc.BASE64Decoder dec = new sun.misc.BASE64Decoder();
+                        String userpassDecoded = new String(dec.decodeBuffer(userpassEncoded));
+                        return "tester:pass".equals(userpassDecoded);
+                    } else {
+                        response.setHeader("WWW-Authenticate", "BASIC realm=\"Testing OSGi\"");
+                        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                        return false;
+                    }
+                }
+
+                public URL getResource(String s) {
+                    return bc.getBundle().getResource(s);
+                }
+
+                public String getMimeType(String s) {
+                    return null;
+                }
+            };
+            hs.registerServlet("/testServlet", new GenericServlet() {
+                @Override
+                public void service(ServletRequest req, ServletResponse resp) throws ServletException, IOException {
+                    HttpServletResponse response = (HttpServletResponse) resp;
+                    response.setStatus(200);
+                    response.getWriter().write("Grizzly");
+                }
+            }, null, context);
+
+            URL url2 = new URL("http://localhost:8080/testServlet");
+            HttpURLConnection conn2 = (HttpURLConnection) url2.openConnection();
+            Assert.assertEquals("Status code should be the same.", 401, conn2.getResponseCode());
+
+            // provide authentication details, should 200
+            Authenticator.setDefault(new Authenticator() {
+                @Override protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication("tester", "pass".toCharArray());
+                }
+            });
+            URL url1 = new URL("http://localhost:8080/testServlet");
+            HttpURLConnection conn1 = (HttpURLConnection) url1.openConnection();
+            Assert.assertEquals("Status code should be the same.", 200, conn1.getResponseCode());
+
+        } finally {
+            tracker.close();
+        }
+    }
+}
