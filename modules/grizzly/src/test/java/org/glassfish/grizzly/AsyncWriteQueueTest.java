bde17f0f7363dacac05c2c6860ee6def6474d61d
oleksiys <oleksiys@04fdb32c-e91d-0410-84f5-98e489d73e21>
2009-03-24 17:02:42 +0000

    AsyncWriteQueue test
    git-svn-id: svn+ssh://svn.java.net/grizzly~svn/branches/2dot0/code@2733 04fdb32c-e91d-0410-84f5-98e489d73e21

---
+++ modules/grizzly/src/test/java/org/glassfish/grizzly/AsyncWriteQueueTest.java
@@ -0,0 +1,171 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 2003-2008 Sun Microsystems, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License. You can obtain
+ * a copy of the License at https://glassfish.dev.java.net/public/CDDL+GPL.html
+ * or glassfish/bootstrap/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at glassfish/bootstrap/legal/LICENSE.txt.
+ * Sun designates this particular file as subject to the "Classpath" exception
+ * as provided by Sun in the GPL Version 2 section of the License file that
+ * accompanied this code.  If applicable, add the following below the License
+ * Header, with the fields enclosed by brackets [] replaced by your own
+ * identifying information: "Portions Copyrighted [year]
+ * [name of copyright owner]"
+ *
+ * Contributor(s):
+ *
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ */
+
+package org.glassfish.grizzly;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import junit.framework.TestCase;
+import org.glassfish.grizzly.filterchain.TransportFilter;
+import org.glassfish.grizzly.memory.MemoryManager;
+import org.glassfish.grizzly.memory.MemoryUtils;
+import org.glassfish.grizzly.nio.transport.TCPNIOConnection;
+import org.glassfish.grizzly.nio.transport.TCPNIOTransport;
+import org.glassfish.grizzly.streams.StreamReader;
+import org.glassfish.grizzly.streams.StreamWriter;
+import org.glassfish.grizzly.util.EchoFilter;
+
+/**
+ *
+ * @author oleksiys
+ */
+public class AsyncWriteQueueTest extends TestCase {
+    public static final int PORT = 7781;
+
+    public void testAsyncWriteQueueEcho() throws Exception {
+        Connection connection = null;
+        StreamReader reader = null;
+        StreamWriter writer = null;
+        
+        final int packetNumber = 20;
+        final int packetSize = 1024;
+
+        TCPNIOTransport transport = TransportFactory.getInstance().createTCPTransport();
+        transport.getFilterChain().add(new TransportFilter());
+        transport.getFilterChain().add(new EchoFilter());
+
+        try {
+            transport.bind(PORT);
+            transport.start();
+            transport.configureBlocking(true);
+
+            Future<Connection> future = transport.connect("localhost", PORT);
+            connection = (TCPNIOConnection) future.get(10, TimeUnit.SECONDS);
+            assertTrue(connection != null);
+
+            connection.setProcessor(null);
+            
+            reader = connection.getStreamReader();
+
+            final Writer asyncQueueWriter = transport.getAsyncQueueIO().getWriter();
+            final MemoryManager mm = transport.getMemoryManager();
+            final Connection con = connection;
+            
+            final CountDownLatch latch = new CountDownLatch(packetNumber);
+
+            final CompletionHandler completionHandler =
+                    new CompletionHandlerAdapter() {
+                @Override
+                public void completed(Connection connection, Object result) {
+                    latch.countDown();
+                }
+            };
+            
+            Collection<Callable<Object>> sendTasks =
+                    new ArrayList<Callable<Object>>(packetNumber + 1);
+            for (int i = 0; i < packetNumber; i++) {
+                final byte b = (byte) i;
+                sendTasks.add(new Callable() {
+                    public Object call() throws Exception {
+                        byte[] originalMessage = new byte[packetSize];
+                        Arrays.fill(originalMessage, b);
+                        Buffer buffer = MemoryUtils.wrap(mm, originalMessage);
+                        try {
+                            asyncQueueWriter.write(con, buffer, completionHandler);
+                        } catch (IOException e) {
+                            assertTrue("IOException occurred", false);
+                        }
+
+                        return null;
+                    }
+                });
+            }
+
+            ExecutorService executorService = Executors.newFixedThreadPool(packetNumber);
+            try {
+                executorService.invokeAll(sendTasks);
+                if (!latch.await(10, TimeUnit.SECONDS)) {
+                    assertTrue("Send timeout!", false);
+                }
+            } finally {
+                executorService.shutdown();
+            }
+
+
+            int responseSize = packetNumber * packetSize;
+            Future readFuture = reader.notifyAvailable(responseSize);
+            assertTrue("Read timeout", readFuture.get(10, TimeUnit.SECONDS) != null);
+
+            byte[] echoMessage = new byte[responseSize];
+            reader.readByteArray(echoMessage);
+
+            // Check interleaving...
+
+            boolean[] isByteUsed = new boolean[packetNumber];
+            int offset = 0;
+            for(int i=0; i<packetNumber; i++) {
+                byte pattern = echoMessage[offset];
+
+                assertEquals("Pattern: " + pattern + " was already used",
+                        false, isByteUsed[pattern]);
+
+                isByteUsed[pattern] = true;
+                for(int j = 0; j < packetSize; j++) {
+                    byte check = echoMessage[offset++];
+                    assertEquals("Echo doesn't match. Offset: " + offset +
+                            " pattern: " + pattern + " found: " + check,
+                            pattern, check);
+                }
+            }
+
+        } finally {
+            if (connection != null) {
+                connection.close();
+            }
+
+            transport.stop();
+            TransportFactory.getInstance().close();
+        }
+    }
+}
