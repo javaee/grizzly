6c8eb5695148cb29be6f7d5074107d3f5616abd0
cheeser <cheeser@04fdb32c-e91d-0410-84f5-98e489d73e21>
2009-04-24 04:32:32 +0000

    hk2 version bump for the new API
    git-svn-id: https://svn.java.net/svn/grizzly~svn/trunk/code@3051 04fdb32c-e91d-0410-84f5-98e489d73e21

--- contribs/config/src/main/java/com/sun/grizzly/config/GrizzlyEmbeddedHttp.java
+++ contribs/config/src/main/java/com/sun/grizzly/config/GrizzlyEmbeddedHttp.java
@@ -256,7 +256,7 @@ public class GrizzlyEmbeddedHttp extends SelectorThread {
         final Protocol httpProtocol = networkListener.findProtocol();
         final Http http = httpProtocol.getHttp();
         final Transport transport = networkListener.findTransport();
-        final ThreadPool pool = networkListener.findThreadPool(habitat);
+        final ThreadPool pool = networkListener.findThreadPool();
 
         setPort(Integer.parseInt(networkListener.getPort()));
         configureHttpListenerProperty(http, transport, httpProtocol.getSsl());
