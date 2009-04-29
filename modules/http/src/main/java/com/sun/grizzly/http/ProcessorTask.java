2d501e85e49624918bd82184940f112f2ca0f012
jfarcand <jfarcand@04fdb32c-e91d-0410-84f5-98e489d73e21>
2009-04-29 23:28:06 +0000

    Possible Fix for https://grizzly.dev.java.net/issues/show_bug.cgi?id=474 ("grizzly hang up after a couple of hours")
    Mark the request as failed. I need more information from Rama about how this can be reproduced, but the current fix/change log level will help.
    
    
    
    
    git-svn-id: https://svn.java.net/svn/grizzly~svn/trunk/code@3095 04fdb32c-e91d-0410-84f5-98e489d73e21

--- modules/http/src/main/java/com/sun/grizzly/http/ProcessorTask.java
+++ modules/http/src/main/java/com/sun/grizzly/http/ProcessorTask.java
@@ -969,10 +969,10 @@ public class ProcessorTask extends TaskBase implements Processor,
             try {
                 outputBuffer.endRequest();
             } catch (IOException e) {
-                if (logger.isLoggable(Level.FINEST)){
-                    logger.log(Level.FINEST,
-                        sm.getString("processorTask.nonBlockingError"), e);
+                if (logger.isLoggable(Level.WARNING)){
+                    logger.log(Level.WARNING,"End Request", e);
                     error = true;
+                    response.setErrorException(e);
                 }
             }
         } else if (actionCode == ActionCode.ACTION_RESET) {
