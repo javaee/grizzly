eedd6b9ee1527b44550396228b5be22c5c13f410
jfarcand <jfarcand@04fdb32c-e91d-0410-84f5-98e489d73e21>
2009-05-07 15:32:25 +0000

    Add Grizzly 1.0 code here.
    git-svn-id: https://svn.java.net/svn/grizzly~svn/trunk/code@3149 04fdb32c-e91d-0410-84f5-98e489d73e21

---
+++ extras/grizzly1.0/src/main/java/com/sun/grizzly/cometd/bayeux/HandshakeResponse.java
@@ -0,0 +1,94 @@
+/*
+ * 
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ * 
+ * Copyright 2007-2008 Sun Microsystems, Inc. All rights reserved.
+ * 
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License. You can obtain
+ * a copy of the License at https://glassfish.dev.java.net/public/CDDL+GPL.html
+ * or glassfish/bootstrap/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ * 
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at glassfish/bootstrap/legal/LICENSE.txt.
+ * Sun designates this particular file as subject to the "Classpath" exception
+ * as provided by Sun in the GPL Version 2 section of the License file that
+ * accompanied this code.  If applicable, add the following below the License
+ * Header, with the fields enclosed by brackets [] replaced by your own
+ * identifying information: "Portions Copyrighted [year]
+ * [name of copyright owner]"
+ * 
+ * Contributor(s):
+ * 
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ *
+ */
+
+package com.sun.grizzly.cometd.bayeux;
+
+/**
+ * @author Jeanfrancois Arcand
+ * @author Shing Wai Chan
+ */
+
+public class HandshakeResponse extends Handshake {
+
+    public HandshakeResponse() {
+        super();
+    }
+
+    public HandshakeResponse(HandshakeRequest req) {
+        super();
+        channel = req.getChannel();
+        id = req.getId();
+        first = req.isFirst();
+        follow = req.isFollow();
+        last = req.isLast();
+    }
+
+    public Boolean getSuccessful() {
+        return successful;
+    }
+
+    public void setSuccessful(Boolean successful) {
+        this.successful = successful;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public Boolean getAuthSuccessful() {
+        return authSuccessful;
+    }
+
+    public void setAuthSuccessful(Boolean authSuccessful) {
+        this.authSuccessful = authSuccessful;
+    }
+
+    @Override
+    public boolean isValid() {
+        return (clientId != null || error != null) && hasValidAdvice() &&
+            super.isValid();
+    }
+
+    public String toJSON() {
+        return (error != null) ? toErrorResponseJSON() : toJSON(true);
+    }
+}
