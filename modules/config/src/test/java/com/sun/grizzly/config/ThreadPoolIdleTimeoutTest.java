4cbef89038181873d8056cba613f374fa9d2c76f
cheeser <cheeser@04fdb32c-e91d-0410-84f5-98e489d73e21>
2009-08-12 14:50:22 +0000

    set the content-type on the response explicitly
    git-svn-id: https://svn.java.net/svn/grizzly~svn/trunk/code@3515 04fdb32c-e91d-0410-84f5-98e489d73e21

--- modules/config/src/test/java/com/sun/grizzly/config/ThreadPoolIdleTimeoutTest.java
+++ modules/config/src/test/java/com/sun/grizzly/config/ThreadPoolIdleTimeoutTest.java
@@ -1,13 +1,7 @@
 package com.sun.grizzly.config;
 
 import java.io.IOException;
-import java.io.OutputStream;
-import java.io.InputStream;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.net.URL;
-import java.net.Socket;
-import java.util.Date;
 import javax.servlet.Servlet;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -37,6 +31,7 @@ public class ThreadPoolIdleTimeoutTest extends GrizzlyServletTestBase {
                 throws ServletException, IOException {
                 try {
                     Thread.sleep(10000);
+                    response.setContentType("text/plain");
                     response.getWriter().println("Here's your content.");
                     response.flushBuffer();
                 } catch (InterruptedException ie) {
@@ -48,15 +43,8 @@ public class ThreadPoolIdleTimeoutTest extends GrizzlyServletTestBase {
 
     @Test
     public void noTimeout() {
-//        goGet();
-//        invokeServlet();
-    }
-
-    private void goGet() {
         try {
-            System.out.println("getting url");
             URL url = new URL("http://" + host + ":" + port + contextPath + servletPath);
-            System.out.println("getting content: " + (new Date()));
             url.getContent();
             Assert.assertTrue(noTimeout, "Servlet should not timeout");
         } catch (IOException ex) {
@@ -65,49 +53,7 @@ public class ThreadPoolIdleTimeoutTest extends GrizzlyServletTestBase {
         } catch (Exception ex) {
             ex.printStackTrace();
             Assert.fail("Servlet should respond", ex);
-        } finally {
-            System.out.println("done getting content: " + (new Date()));
         }
     }
-
-    private void invokeServlet() {
-        try {
-            sock = new Socket(host, port);
-            try {
-                OutputStream os = sock.getOutputStream();
-                String get = "GET " + contextPath + servletPath + " HTTP/1.1\n";
-                os.write(get.getBytes());
-                String hostHeader = "Host: " + host + ":" + port + "\n";
-                os.write(hostHeader.getBytes());
-                os.write("\n".getBytes());
-                InputStream is = sock.getInputStream();
-                BufferedReader bis = new BufferedReader(new InputStreamReader(is));
-                boolean responseOK = false;
-                boolean contentLenFound = false;
-                String line;
-                while ((line = bis.readLine()) != null) {
-                    System.out.println(line);
-                    responseOK |= line.contains("HTTP/1.1 200 OK");
-                    contentLenFound |= line.contains("Content-Length:") || line.contains("Content-length:");
-                }
-                if (responseOK && contentLenFound) {
-                    Assert.assertTrue(true, "Response OK");
-                } else if (!responseOK) {
-                    Assert.fail("Wrong response code, expected 200 OK");
-                } else {
-                    Assert.fail("Missing Content-Length response header");
-                }
-            } finally {
-                if (sock != null) {
-                    sock.close();
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-            Assert.fail(e.getMessage(), e);
-        }
-    }
-
-    Socket sock = null;
 
 }
