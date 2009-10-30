8d62b9c628bb32fdca6be9905c8adf67e19f44d8
jfarcand <jfarcand@04fdb32c-e91d-0410-84f5-98e489d73e21>
2009-10-30 17:03:40 +0000

    Port fix
    git-svn-id: https://svn.java.net/svn/grizzly~svn/tags/1_9_18f@3836 04fdb32c-e91d-0410-84f5-98e489d73e21

--- modules/http/src/main/java/com/sun/grizzly/http/SelectorThreadConfig.java
+++ modules/http/src/main/java/com/sun/grizzly/http/SelectorThreadConfig.java
@@ -1,9 +1,9 @@
 /*
- *
+ * 
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
- *
+ * 
  * Copyright 2007-2008 Sun Microsystems, Inc. All rights reserved.
- *
+ * 
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
  * and Distribution License("CDDL") (collectively, the "License").  You
@@ -11,7 +11,7 @@
  * a copy of the License at https://glassfish.dev.java.net/public/CDDL+GPL.html
  * or glassfish/bootstrap/legal/LICENSE.txt.  See the License for the specific
  * language governing permissions and limitations under the License.
- *
+ * 
  * When distributing the software, include this License Header Notice in each
  * file and include the License file at glassfish/bootstrap/legal/LICENSE.txt.
  * Sun designates this particular file as subject to the "Classpath" exception
@@ -20,9 +20,9 @@
  * Header, with the fields enclosed by brackets [] replaced by your own
  * identifying information: "Portions Copyrighted [year]
  * [name of copyright owner]"
- *
+ * 
  * Contributor(s):
- *
+ * 
  * If you wish your version of this file to be governed by only the CDDL or
  * only the GPL Version 2, indicate your decision by adding "[Contributor]
  * elects to include this software in this distribution under the [CDDL or GPL
@@ -50,29 +50,29 @@ import java.util.StringTokenizer;
 import java.util.concurrent.ExecutorService;
 import java.util.logging.Level;
 
-public class SelectorThreadConfig{
-
+public class SelectorThreadConfig{  
+    
     /**
      * System property for the selector timeout value.
      */
-    private static final String SELECTOR_TIMEOUT =
+    private static final String SELECTOR_TIMEOUT = 
                 "com.sun.grizzly.selector.timeout";
-
+    
     /**
-     * The minimum number of threads used when creating a new
+     * The minimum number of threads used when creating a new 
      * {@link ExecutorService}
      */
     private static final String MIN_THREAD=
                "com.sun.grizzly.minWorkerThreads";
-
+    
     /**
-     * The maximum number of threads used when creating a new
+     * The maximum number of threads used when creating a new 
      * {@link ExecutorService}
      */
     private static final String MAX_THREAD=
                "com.sun.grizzly.maxThreads";
-
-
+    
+    
     /**
      * Property used to turn on/off NIO blocking mode.
      */
@@ -81,7 +81,7 @@ public class SelectorThreadConfig{
 
 
     private final static String MAX_KEEP_ALIVE_REQUEST =
-               "com.sun.grizzly.maxKeepAliveRequests";
+               "com.sun.grizzly.maxKeepAliveRequests"; 
 
 
     /**
@@ -89,62 +89,62 @@ public class SelectorThreadConfig{
      * direct {@link ByteBuffer} or not.
      */
     private final static String DIRECT_BYTE_BUFFER_READ =
-               "com.sun.grizzly.useDirectByteBuffer";
-
+               "com.sun.grizzly.useDirectByteBuffer";    
+ 
     /**
-     * Always attach a {@link ProcessorTask} when creating
+     * Always attach a {@link ProcessorTask} when creating 
      * a <code>ReadTask</code>
      */
     private final static String THREAD_POOL_CLASS =
                "com.sun.grizzly.threadPoolClass";
 
     private final static String MAX_SELECTOR_READ_THREAD=
-               "com.sun.grizzly.maxSelectorReadThread";
-
+               "com.sun.grizzly.maxSelectorReadThread"; 
+    
     private final static String BYTE_BUFFER_VIEW =
-               "com.sun.grizzly.useByteBufferView";
+               "com.sun.grizzly.useByteBufferView"; 
 
     private final static String ALGORITHM_CLASS_NAME=
         "com.sun.grizzly.algorithmClassName";
-
-    private final static String MAX_SELECTOR =
+    
+    private final static String MAX_SELECTOR = 
         "com.sun.grizzly.maxSelectors";
-
-    private final static String FACTORY_TIMEOUT =
+    
+    private final static String FACTORY_TIMEOUT = 
         "com.sun.grizzly.factoryTimeout";
-
-
+    
+    
     private final static String ASYNCH_HANDLER_CLASS =
-        "com.sun.grizzly.asyncHandlerClass";
-
+        "com.sun.grizzly.asyncHandlerClass";   
+    
     private final static String ASYNCH_HANDLER_PORT =
-        "com.sun.grizzly.asyncHandler.ports";
-
-    private final static String SNOOP_LOGGING =
+        "com.sun.grizzly.asyncHandler.ports";   
+    
+    private final static String SNOOP_LOGGING = 
         "com.sun.grizzly.enableSnoop";
 
-    private final static String TEMPORARY_SELECTOR_TIMEOUT =
-        "com.sun.grizzly.readTimeout";
-
-    private final static String WRITE_TIMEOUT =
-        "com.sun.grizzly.writeTimeout";
-
-    private final static String IDLE_THREAD_TIMEOUT =
-        "com.sun.grizzly.idleThreadTimeout";
-
-    private final static String BUFFER_RESPONSE =
-        "com.sun.grizzly.http.bufferResponse";
-
-    private final static String OOBInline =
-        "com.sun.enterprise.web.connector.grizzly.OOBInline";
-
+    private final static String TEMPORARY_SELECTOR_TIMEOUT = 
+        "com.sun.grizzly.readTimeout"; 
+    
+    private final static String WRITE_TIMEOUT = 
+        "com.sun.grizzly.writeTimeout";     
+    
+    private final static String IDLE_THREAD_TIMEOUT = 
+        "com.sun.grizzly.idleThreadTimeout";    
+    
+    private final static String BUFFER_RESPONSE = 
+        "com.sun.grizzly.http.bufferResponse"; 
+    
+    private final static String OOBInline = 
+        "com.sun.grizzly.OOBInline"; 
+    
     private final static String MAX_BUFFERED_BYTES =
         "com.sun.grizzly.maxBufferedBytes";
-
+    
     private final static String USE_FILE_CACHE =
         "com.sun.grizzly.useFileCache";
 
-
+    
     private final static String IS_ASYNC_HTTP_WRITE =
             "com.sun.grizzly.http.asyncwrite.enabled";
 
@@ -156,15 +156,21 @@ public class SelectorThreadConfig{
 
     private final static String SSL_CONFIGURATION_SSLIMPL =
             "com.sun.grizzly.ssl.sslImplementation";
-
+    
    private final static String ASYNC_HANDLER_ENABLED_CONTEXT_PATH =
            "com.sun.grizzly.arp.asyncHandlerContextPath";
+
+    /**
+     * Do not bind on occupied port
+     */
+    public final static String REUSE_ADRESS =
+            "com.sun.grizzly.http.reuseAddress";
 
    /**
      * The string manager for this package.
      */
     protected static final StringManager sm =
-        StringManager.getManager(Constants.Package);
+        StringManager.getManager(Constants.Package);    
 // --------------------------------------------------------- Static -----//
 
 
@@ -188,10 +194,10 @@ public class SelectorThreadConfig{
                         System.getProperty(TEMPORARY_SELECTOR_TIMEOUT));
                 InputReader.setDefaultReadTimeout(timeout);
             } catch (NumberFormatException ex){
-                SelectorThread.logger().log(Level.WARNING,
+                SelectorThread.logger().log(Level.WARNING, 
                                             sm.getString("selectorThread.invalidReadTimeout"));
             }
-        }
+        }             
 
         if (System.getProperty(WRITE_TIMEOUT) != null){
             try{
@@ -199,22 +205,22 @@ public class SelectorThreadConfig{
                         System.getProperty(WRITE_TIMEOUT));
                 OutputWriter.setDefaultWriteTimeout(timeout);
             } catch (NumberFormatException ex){
-                SelectorThread.logger().log(Level.WARNING,
+                SelectorThread.logger().log(Level.WARNING, 
                                             sm.getString("selectorThread.invalidWriteTimeout"));
             }
-        }
-
+        } 
+        
         if (System.getProperty(IDLE_THREAD_TIMEOUT) != null){
             try{
                 int timeout =  Integer.parseInt(
                         System.getProperty(IDLE_THREAD_TIMEOUT));
                 selectorThread.setTransactionTimeout(timeout);
             } catch (NumberFormatException ex){
-                SelectorThread.logger().log(Level.WARNING,
+                SelectorThread.logger().log(Level.WARNING, 
                                             sm.getString("selectorThread.invalidWriteTimeout"));
             }
         }
-
+        
         String threadPoolClassname = null;
         int minWorkerThreads = StatsThreadPool.DEFAULT_MIN_THREAD_COUNT;
         int maxWorkerThreads = StatsThreadPool.DEFAULT_MAX_THREAD_COUNT;
@@ -232,9 +238,9 @@ public class SelectorThreadConfig{
             } catch (NumberFormatException ex){
                 SelectorThread.logger().log(Level.WARNING,sm.getString("selectorThread.invalidMinThreads"));
             }
-        }
-
-
+        }    
+        
+        
         if (System.getProperty(MAX_THREAD) != null){
             try{
                 maxWorkerThreads =
@@ -243,37 +249,39 @@ public class SelectorThreadConfig{
             } catch (NumberFormatException ex){
                 SelectorThread.logger().log(Level.WARNING, sm.getString("selectorThread.invalidMaxThreads"));
             }
-        }
-
+        }  
+        
         if (System.getProperty(DISPLAY_CONFIGURATION)!= null){
-            selectorThread.displayConfiguration =
+            selectorThread.displayConfiguration = 
                 Boolean.valueOf(System.getProperty(DISPLAY_CONFIGURATION))
                                                                 .booleanValue();
         }
 
+        selectorThread.setReuseAddress(Boolean.getBoolean(REUSE_ADRESS));
+        
         if (System.getProperty(ASYNCH_HANDLER_PORT) != null){
             String ports = System.getProperty(ASYNCH_HANDLER_PORT);
             StringTokenizer st = new StringTokenizer(ports,",");
             while(st.hasMoreTokens()){
-
+                
                 if ( st.nextToken()
                         .equals(String.valueOf(selectorThread.getPort()))
                         && System.getProperty(ASYNCH_HANDLER_CLASS)!= null){
-
+                    
                     selectorThread.asyncHandler = (AsyncHandler)
                         ClassLoaderUtil.load(
-                            System.getProperty(ASYNCH_HANDLER_CLASS));
+                            System.getProperty(ASYNCH_HANDLER_CLASS)); 
                     selectorThread.asyncExecution = true;
                 }
             }
-        }
-
+        }           
+        
         if (System.getProperty(DIRECT_BYTE_BUFFER_READ)!= null){
-            selectorThread.useDirectByteBuffer =
+            selectorThread.useDirectByteBuffer = 
                 Boolean.valueOf(
                     System.getProperty(DIRECT_BYTE_BUFFER_READ)).booleanValue();
-        }
-
+        }        
+       
         if (System.getProperty(MAX_KEEP_ALIVE_REQUEST) != null){
             try{
                 selectorThread.setMaxKeepAliveRequests(
@@ -282,27 +290,27 @@ public class SelectorThreadConfig{
                 ;
             }
         }
-
+        
         if (System.getProperty(ALGORITHM_CLASS_NAME)!= null){
-            selectorThread.algorithmClassName =
+            selectorThread.algorithmClassName = 
                                       System.getProperty(ALGORITHM_CLASS_NAME);
-        }
+        }   
 
         if (System.getProperty(BYTE_BUFFER_VIEW)!= null){
-            selectorThread.useByteBufferView =
+            selectorThread.useByteBufferView = 
                 Boolean.valueOf(
                             System.getProperty(BYTE_BUFFER_VIEW)).booleanValue();
-        }
-
+        }    
+             
         if (System.getProperty(MAX_SELECTOR_READ_THREAD) != null){
             try{
-                selectorThread.readThreadsCount =
+                selectorThread.readThreadsCount = 
                   Integer.parseInt(System.getProperty(MAX_SELECTOR_READ_THREAD));
             } catch (NumberFormatException ex){
                 ;
             }
         }
-
+        
         if (System.getProperty(MAX_SELECTOR) != null){
             try{
                 SelectorFactory.setMaxSelectors(
@@ -312,28 +320,28 @@ public class SelectorThreadConfig{
             } catch (IOException ex) {
                 ;
             }
-        }
-
+        } 
+        
         if (System.getProperty(SNOOP_LOGGING)!= null){
             SelectorThread.setEnableNioLogging(
                 Boolean.valueOf(
                             System.getProperty(SNOOP_LOGGING)).booleanValue());
-        }
-
+        }   
+        
         if (System.getProperty(BUFFER_RESPONSE)!= null){
-            selectorThread.setBufferResponse(
+            selectorThread.setBufferResponse( 
                 Boolean.valueOf(
                             System.getProperty(BUFFER_RESPONSE)).booleanValue());
-        }
-
+        }    
+        
         if (System.getProperty(OOBInline)!= null){
-            selectorThread.oOBInline =
+            selectorThread.oOBInline = 
                 Boolean.valueOf(System.getProperty(OOBInline)).booleanValue();
         }
-
+        
         if (System.getProperty(MAX_BUFFERED_BYTES) != null){
             try{
-                SocketChannelOutputBuffer.setMaxBufferedBytes(
+                SocketChannelOutputBuffer.setMaxBufferedBytes( 
                     Integer.parseInt(System.getProperty(MAX_BUFFERED_BYTES)));
             } catch (NumberFormatException ex){
                 SelectorThread.logger().log(Level.WARNING, sm.getString("selectorThread.invalidMaxBufferedBytes"));
@@ -364,7 +372,7 @@ public class SelectorThreadConfig{
                     ((SSLSelectorThread)selectorThread).setWantClientAuth(true);
                 } else if (auth.trim().equalsIgnoreCase("need")){
                     ((SSLSelectorThread)selectorThread).setNeedClientAuth(true);
-                }
+                } 
             }
         }
 
@@ -385,7 +393,7 @@ public class SelectorThreadConfig{
         }
     }
 
-
+    
     /**
      * Configure properties on {@link SelectorThread}
      */
