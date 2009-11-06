4817f5722dad763b4bd72e9e095d8b0fe41bf58c
cheeser <cheeser@04fdb32c-e91d-0410-84f5-98e489d73e21>
2009-11-06 14:38:49 +0000

    adding a default value
    git-svn-id: https://svn.java.net/svn/grizzly~svn/trunk/code@3872 04fdb32c-e91d-0410-84f5-98e489d73e21

--- modules/config/src/main/java/com/sun/grizzly/config/dom/Ssl.java
+++ modules/config/src/main/java/com/sun/grizzly/config/dom/Ssl.java
@@ -53,7 +53,7 @@ public interface Ssl extends ConfigBeanProxy, Injectable, PropertyBag {
      * Nickname of the server certificate in the certificate database or the PKCS#11 token. In the certificate, the name
      * format is tokenname:nickname. Including the tokenname: part of the name in this attribute is optional.
      */
-    @Attribute(required = true)
+    @Attribute(required = true, defaultValue="s1as")
     String getCertNickname();
 
     void setCertNickname(String value);
@@ -176,7 +176,7 @@ public interface Ssl extends ConfigBeanProxy, Injectable, PropertyBag {
 
     void setTrustAlgorithm(String algorithm);
 
-    @Attribute(dataType = Integer.class)
+    @Attribute(dataType = Integer.class, defaultValue = "5")
     String getTrustMaxCertLengthBytes();
 
     void setTrustMaxCertLengthBytes(String maxLength);
