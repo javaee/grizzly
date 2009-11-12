51eb5fd1068a96d0553177b923eb6cc34871aa63
cheeser <cheeser@04fdb32c-e91d-0410-84f5-98e489d73e21>
2009-11-12 16:27:58 +0000

    bump to hk2 0.4.15 for https://glassfish.dev.java.net/issues/show_bug.cgi?id=10946
    git-svn-id: https://svn.java.net/svn/grizzly~svn/trunk/code@3912 04fdb32c-e91d-0410-84f5-98e489d73e21

--- modules/config/src/main/java/com/sun/grizzly/config/dom/NetworkAddress.java
+++ modules/config/src/main/java/com/sun/grizzly/config/dom/NetworkAddress.java
@@ -7,7 +7,6 @@ import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 import javax.validation.Constraint;
-import javax.validation.ConstraintPayload;
 
 @Retention(RUNTIME)
 @Target({FIELD, METHOD})
@@ -17,7 +16,4 @@ public @interface NetworkAddress {
     String message() default "must be a valid network address";
 
     Class<?>[] groups() default {};
-
-    Class<? extends ConstraintPayload>[] payload() default {};
-
 }
