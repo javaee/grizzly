b359e1e087fd7b6dc2af22e7449a4bf033a03b60
rlubke <rlubke@04fdb32c-e91d-0410-84f5-98e489d73e21>
2010-05-27 22:56:35 +0000

    Update copyright years.
    git-svn-id: https://svn.java.net/svn/grizzly~svn/trunk/code@4586 04fdb32c-e91d-0410-84f5-98e489d73e21

--- modules/utils/src/main/java/com/sun/grizzly/util/ThreadAttachment.java
+++ modules/utils/src/main/java/com/sun/grizzly/util/ThreadAttachment.java
@@ -2,7 +2,7 @@
  *
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright 2007-2008 Sun Microsystems, Inc. All rights reserved.
+ * Copyright 2007-2010 Sun Microsystems, Inc. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -105,7 +105,7 @@ public class ThreadAttachment extends SelectionKeyActionAttachment
 
 
     /**
-     * The current {@link ThreadFactory} used to execute this instance.
+     * The current {@link Factory} used to execute this instance.
      */
     private Thread activeThread = null;
 
