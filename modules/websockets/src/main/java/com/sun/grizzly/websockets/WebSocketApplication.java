022397110cc12256ca3ef1296dd85e38e3b7a880
oleksiys <oleksiys@04fdb32c-e91d-0410-84f5-98e489d73e21>
2010-06-22 13:47:47 +0000

    + make impl. threadsafe + fix issue #838 https://grizzly.dev.java.net/issues/show_bug.cgi?id=838 "expose list of connected to sockets"
    git-svn-id: https://svn.java.net/svn/grizzly~svn/trunk/code@4670 04fdb32c-e91d-0410-84f5-98e489d73e21

--- modules/websockets/src/main/java/com/sun/grizzly/websockets/WebSocketApplication.java
+++ modules/websockets/src/main/java/com/sun/grizzly/websockets/WebSocketApplication.java
@@ -4,16 +4,29 @@ import com.sun.grizzly.tcp.Request;
 import com.sun.grizzly.tcp.Response;
 
 import java.io.IOException;
-import java.nio.channels.Channel;
-import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 public abstract class WebSocketApplication implements WebSocketListener {
-    private final Set<WebSocket> sockets = new HashSet<WebSocket>();
-    private final Set<WebSocketListener> listeners = new HashSet<WebSocketListener>();
-    
+    private final Map<WebSocket, Boolean> sockets =
+            new ConcurrentHashMap<WebSocket, Boolean>();
+
+    private final Map<WebSocketListener, Boolean> listeners =
+            new ConcurrentHashMap<WebSocketListener, Boolean>();
+
+    /**
+     * Returns a set of {@link WebSocket}s, registered with the application.
+     * The returned set is unmodifiable, the possible modifications may cause exceptions.
+     *
+     * @return a set of {@link WebSocket}s, registered with the application.
+     */
+    protected Set<WebSocket> getWebSockets() {
+        return sockets.keySet();
+    }
+
     public boolean add(WebSocket socket) {
-        return sockets.add(socket);
+        return sockets.put(socket, Boolean.TRUE);
     }
     
     public boolean remove(WebSocket socket) {
@@ -21,7 +34,7 @@ public abstract class WebSocketApplication implements WebSocketListener {
     }
     
     public boolean add(WebSocketListener listener) {
-        return listeners.add(listener);
+        return listeners.put(listener, Boolean.TRUE);
     }
     
     public boolean remove(WebSocketListener listener) {
