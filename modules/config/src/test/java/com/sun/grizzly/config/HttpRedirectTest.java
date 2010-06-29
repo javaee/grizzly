36494bc7c4cae779ad0959d623d47013f15a35e1
rlubke <rlubke@04fdb32c-e91d-0410-84f5-98e489d73e21>
2010-06-29 16:24:47 +0000

    Refactor test.
    git-svn-id: https://svn.java.net/svn/grizzly~svn/trunk/code@4702 04fdb32c-e91d-0410-84f5-98e489d73e21

--- modules/config/src/test/java/com/sun/grizzly/config/HttpRedirectTest.java
+++ modules/config/src/test/java/com/sun/grizzly/config/HttpRedirectTest.java
@@ -43,13 +43,11 @@ import org.testng.annotations.Test;
 
 import javax.net.SocketFactory;
 import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLSocketFactory;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -57,7 +55,6 @@ import java.io.OutputStream;
 import java.net.Socket;
 import java.security.SecureRandom;
 import java.security.cert.X509Certificate;
-import java.util.logging.Logger;
 
 @Test
 public class HttpRedirectTest extends BaseGrizzlyConfigTest {
@@ -66,228 +63,100 @@ public class HttpRedirectTest extends BaseGrizzlyConfigTest {
     // ------------------------------------------------------------ Test Methods
 
 
-    public void legacyHttpToHttpsRedirect() {
-        GrizzlyConfig grizzlyConfig = new GrizzlyConfig("legacy-http-https-redirect.xml");
-        grizzlyConfig.setupNetwork();
-        int count = 0;
-        for (GrizzlyServiceListener listener : grizzlyConfig.getListeners()) {
-            setRootFolder(listener, count++);
-        }
-
-        try {
-            Socket s = new Socket("localhost", 48480);
-            OutputStream out = s.getOutputStream();
-            out.write("GET / HTTP/1.1\n".getBytes());
-            out.write(("Host: localhost:" + 48480 + "\n").getBytes());
-            out.write("\n".getBytes());
-            out.flush();
-            InputStream in = s.getInputStream();
-            BufferedReader br = new BufferedReader(new InputStreamReader(in));
-            boolean found = false;
-            for (String line = br.readLine(); line != null; line = br.readLine()) {
-                if (line.length() > 0 && line.toLowerCase().charAt(0) == 'l') {
-                    Assert.assertEquals(line.toLowerCase(), "location: https://localhost:48480/");
-                    found = true;
-                    break;
-                }
-            }
-            if (!found) {
-                Assert.fail("Unable to find Location header in response - no redirect occurred.");
-            }
-        } catch (Exception e) {
-            Assert.fail(e.toString(), e);
-        } finally {
-            grizzlyConfig.shutdownNetwork();
-        }
+    public void legacyHttpToHttpsRedirect() throws IOException {
+        doTest(SocketFactory.getDefault(),
+               "legacy-http-https-redirect.xml",
+                "localhost",
+                48480,
+                "location: https://localhost:48480/");
     }
 
 
-    public void legacyHttpsToHttpRedirect() {
-        GrizzlyConfig grizzlyConfig = new GrizzlyConfig("legacy-https-http-redirect.xml");
-        grizzlyConfig.setupNetwork();
-        int count = 0;
-        for (GrizzlyServiceListener listener : grizzlyConfig.getListeners()) {
-            setRootFolder(listener, count++);
-        }
-
-        try {
-            SocketFactory f = getSSLSocketFactory();
-            Socket s = f.createSocket("localhost", 48480);
-            s.setSoTimeout(50000);
-            OutputStream out = s.getOutputStream();
-            out.write("GET / HTTP/1.1\n".getBytes());
-            out.write(("Host: localhost:" + 48480 + "\n").getBytes());
-            out.write("\n".getBytes());
-            out.flush();
-            InputStream in = s.getInputStream();
-            BufferedReader br = new BufferedReader(new InputStreamReader(in));
-            boolean found = false;
-            for (String line = br.readLine(); line != null; line = br.readLine()) {
-                if (line.length() > 0 && line.toLowerCase().charAt(0) == 'l') {
-                    Assert.assertEquals(line.toLowerCase(), "location: http://localhost:48480/");
-                    found = true;
-                    break;
-                }
-            }
-            if (!found) {
-                Assert.fail("Unable to find Location header in response - no redirect occurred.");
-            }
-        } catch (Exception e) {
-            Assert.fail(e.toString(), e);
-        } finally {
-            grizzlyConfig.shutdownNetwork();
-        }
+    public void legacyHttpsToHttpRedirect() throws IOException {
+        doTest(getSSLSocketFactory(),
+               "legacy-https-http-redirect.xml",
+                "localhost",
+                48480,
+                "location: http://localhost:48480/");
     }
 
 
-    public void httpToHttpsSamePortRedirect() {
-        GrizzlyConfig grizzlyConfig = new GrizzlyConfig("http-https-redirect-same-port.xml");
-        grizzlyConfig.setupNetwork();
-        int count = 0;
-        for (GrizzlyServiceListener listener : grizzlyConfig.getListeners()) {
-            setRootFolder(listener, count++);
-        }
-
-        try {
-            Socket s = new Socket("localhost", 48480);
-            OutputStream out = s.getOutputStream();
-            out.write("GET / HTTP/1.1\n".getBytes());
-            out.write(("Host: localhost:" + 48480 + "\n").getBytes());
-            out.write("\n".getBytes());
-            out.flush();
-            InputStream in = s.getInputStream();
-            BufferedReader br = new BufferedReader(new InputStreamReader(in));
-            boolean found = false;
-            for (String line = br.readLine(); line != null; line = br.readLine()) {
-                if (line.length() > 0 && line.toLowerCase().charAt(0) == 'l') {
-                    Assert.assertEquals(line.toLowerCase(), "location: https://localhost:48480/");
-                    found = true;
-                    break;
-                }
-            }
-            if (!found) {
-                Assert.fail("Unable to find Location header in response - no redirect occurred.");
-            }
-        } catch (Exception e) {
-            Assert.fail(e.toString(), e);
-        } finally {
-            grizzlyConfig.shutdownNetwork();
-        }
+    public void httpToHttpsSamePortRedirect() throws IOException {
+        doTest(SocketFactory.getDefault(),
+               "http-https-redirect-same-port.xml",
+                "localhost",
+                48480,
+                "location: https://localhost:48480/");
     }
 
 
-    public void httpsToHttpSamePortRedirect() {
-        GrizzlyConfig grizzlyConfig = new GrizzlyConfig("https-http-redirect-same-port.xml");
-        grizzlyConfig.setupNetwork();
-        int count = 0;
-        for (GrizzlyServiceListener listener : grizzlyConfig.getListeners()) {
-            setRootFolder(listener, count++);
-        }
-
-        try {
-            SocketFactory f = getSSLSocketFactory();
-            Socket s = f.createSocket("localhost", 48480);
-            s.setSoTimeout(50000);
-            OutputStream out = s.getOutputStream();
-            out.write("GET / HTTP/1.1\n".getBytes());
-            out.write(("Host: localhost:" + 48480 + "\n").getBytes());
-            out.write("\n".getBytes());
-            out.flush();
-            InputStream in = s.getInputStream();
-            BufferedReader br = new BufferedReader(new InputStreamReader(in));
-            boolean found = false;
-            for (String line = br.readLine(); line != null; line = br.readLine()) {
-                if (line.length() > 0 && line.toLowerCase().charAt(0) == 'l') {
-                    Assert.assertEquals(line.toLowerCase(), "location: http://localhost:48480/");
-                    found = true;
-                    break;
-                }
-            }
-            if (!found) {
-                Assert.fail("Unable to find Location header in response - no redirect occurred.");
-            }
-        } catch (Exception e) {
-            Assert.fail(e.toString(), e);
-        } finally {
-            grizzlyConfig.shutdownNetwork();
-        }
+    public void httpsToHttpSamePortRedirect() throws IOException {
+        doTest(getSSLSocketFactory(),
+               "https-http-redirect-same-port.xml",
+                "localhost",
+                48480,
+                "location: http://localhost:48480/");
     }
 
 
-    public void httpToHttpsDifferentPortRedirect() {
-        GrizzlyConfig grizzlyConfig = new GrizzlyConfig("http-https-redirect-different-port.xml");
-        grizzlyConfig.setupNetwork();
-        int count = 0;
-        for (GrizzlyServiceListener listener : grizzlyConfig.getListeners()) {
-            setRootFolder(listener, count++);
-        }
-
-        try {
-            Socket s = new Socket("localhost", 48480);
-            OutputStream out = s.getOutputStream();
-            out.write("GET / HTTP/1.1\n".getBytes());
-            out.write(("Host: localhost:" + 48480 + "\n").getBytes());
-            out.write("\n".getBytes());
-            out.flush();
-            InputStream in = s.getInputStream();
-            BufferedReader br = new BufferedReader(new InputStreamReader(in));
-            boolean found = false;
-            for (String line = br.readLine(); line != null; line = br.readLine()) {
-                if (line.length() > 0 && line.toLowerCase().charAt(0) == 'l') {
-                    Assert.assertEquals(line.toLowerCase(), "location: https://localhost:48481/");
-                    found = true;
-                    break;
-                }
-            }
-            if (!found) {
-                Assert.fail("Unable to find Location header in response - no redirect occurred.");
-            }
-        } catch (Exception e) {
-            Assert.fail(e.toString(), e);
-        } finally {
-            grizzlyConfig.shutdownNetwork();
-        }
+    public void httpToHttpsDifferentPortRedirect() throws IOException {
+        doTest(SocketFactory.getDefault(),
+               "http-https-redirect-different-port.xml",
+                "localhost",
+                48480,
+                "location: https://localhost:48481/");
     }
 
 
-    public void httpsToHttpDifferentPortRedirect() {
-        GrizzlyConfig grizzlyConfig = new GrizzlyConfig("https-http-redirect-different-port.xml");
-        grizzlyConfig.setupNetwork();
-        int count = 0;
-        for (GrizzlyServiceListener listener : grizzlyConfig.getListeners()) {
-            setRootFolder(listener, count++);
-        }
-
-        try {
-            SocketFactory f = getSSLSocketFactory();
-            Socket s = f.createSocket("localhost", 48480);
-            s.setSoTimeout(50000);
-            OutputStream out = s.getOutputStream();
-            out.write("GET / HTTP/1.1\n".getBytes());
-            out.write(("Host: localhost:" + 48480 + "\n").getBytes());
-            out.write("\n".getBytes());
-            out.flush();
-            InputStream in = s.getInputStream();
-            BufferedReader br = new BufferedReader(new InputStreamReader(in));
-            boolean found = false;
-            for (String line = br.readLine(); line != null; line = br.readLine()) {
-                if (line.length() > 0 && line.toLowerCase().charAt(0) == 'l') {
-                    Assert.assertEquals(line.toLowerCase(), "location: http://localhost:48481/");
-                    found = true;
-                    break;
-                }
-            }
-            if (!found) {
-                Assert.fail("Unable to find Location header in response - no redirect occurred.");
-            }
-        } catch (Exception e) {
-            Assert.fail(e.toString(), e);
-        } finally {
-            grizzlyConfig.shutdownNetwork();
-        }
+    public void httpsToHttpDifferentPortRedirect() throws IOException {
+        doTest(getSSLSocketFactory(),
+               "https-http-redirect-different-port.xml",
+                "localhost",
+                48480,
+                "location: http://localhost:48481/");
     }
 
     // --------------------------------------------------------- Private Methods
+
+
+    private void doTest(SocketFactory socketFactory,
+                        String configFile,
+                        String host,
+                        int port,
+                        String expectedLocation) {
+        GrizzlyConfig grizzlyConfig = new GrizzlyConfig(configFile);
+        grizzlyConfig.setupNetwork();
+        int count = 0;
+        for (GrizzlyServiceListener listener : grizzlyConfig.getListeners()) {
+            setRootFolder(listener, count++);
+        }
+
+        try {
+            Socket s = socketFactory.createSocket("localhost", 48480);
+            OutputStream out = s.getOutputStream();
+            out.write("GET / HTTP/1.1\n".getBytes());
+            out.write(("Host: " + host + ':' + Integer.toString(port) + '\n').getBytes());
+            out.write("\n".getBytes());
+            out.flush();
+            InputStream in = s.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            boolean found = false;
+            for (String line = br.readLine(); line != null; line = br.readLine()) {
+                if (line.length() > 0 && line.toLowerCase().charAt(0) == 'l') {
+                    Assert.assertEquals(line.toLowerCase(), expectedLocation);
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                Assert.fail("Unable to find Location header in response - no redirect occurred.");
+            }
+        } catch (Exception e) {
+            Assert.fail(e.toString(), e);
+        } finally {
+            grizzlyConfig.shutdownNetwork();
+        }
+    }
 
     @Override
     protected void setRootFolder(final GrizzlyServiceListener listener, final int count) {
