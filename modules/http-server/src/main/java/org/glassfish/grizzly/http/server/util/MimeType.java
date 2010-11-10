9560a760a8f754ca241acc81a0084d95d5a8817d
rlubke <rlubke@04fdb32c-e91d-0410-84f5-98e489d73e21>
2010-11-10 23:32:02 +0000

    Resolve compilation issue.
    git-svn-id: svn+ssh://svn.java.net/grizzly~svn/branches/2dot0/code@5373 04fdb32c-e91d-0410-84f5-98e489d73e21

--- modules/http-server/src/main/java/org/glassfish/grizzly/http/server/util/MimeType.java
+++ modules/http-server/src/main/java/org/glassfish/grizzly/http/server/util/MimeType.java
@@ -37,7 +37,7 @@
  * only if the new code is made subject to such option by the copyright
  * holder.
  */
-package org.glassfish.grizzly.http.util;
+package org.glassfish.grizzly.http.server.util;
 
 import java.util.HashMap;
 import java.util.Map;
