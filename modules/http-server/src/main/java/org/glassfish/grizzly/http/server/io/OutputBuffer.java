74ca770758243df10192e758a6c3d38e4f099561
oleksiys <oleksiys@04fdb32c-e91d-0410-84f5-98e489d73e21>
2010-12-02 09:43:17 +0000

    + one more doCommit() optimization
    git-svn-id: svn+ssh://svn.java.net/grizzly~svn/branches/2dot0/code@5453 04fdb32c-e91d-0410-84f5-98e489d73e21

--- modules/http-server/src/main/java/org/glassfish/grizzly/http/server/io/OutputBuffer.java
+++ modules/http-server/src/main/java/org/glassfish/grizzly/http/server/io/OutputBuffer.java
@@ -608,7 +608,6 @@ public class OutputBuffer {
     
     private boolean doCommit() throws IOException {
 
-        handleAsyncErrors();
         if (!committed) {
             committed = true;
             return true;
