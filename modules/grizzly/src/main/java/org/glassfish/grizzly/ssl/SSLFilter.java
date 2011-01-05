15f43fcaa66c5886a158c5c708298b382254ba09
rlubke <rlubke@04fdb32c-e91d-0410-84f5-98e489d73e21>
2011-01-05 22:09:46 +0000

    javadoc fix.
    git-svn-id: svn+ssh://svn.java.net/grizzly~svn/branches/2dot0/code@5631 04fdb32c-e91d-0410-84f5-98e489d73e21

--- modules/grizzly/src/main/java/org/glassfish/grizzly/ssl/SSLFilter.java
+++ modules/grizzly/src/main/java/org/glassfish/grizzly/ssl/SSLFilter.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2008-2010 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2008-2011 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -137,7 +137,7 @@ public final class SSLFilter extends AbstractCodecFilter<Buffer, Buffer> {
      * data before SSL handshake completes, so the data should be stored and
      * sent on wire once handshake will be completed.
      *
-     * @param maxQueuedWrites maximum number of pending writes that may be
+     * @param maxPendingBytes maximum number of pending writes that may be
      *  queued for a particular {@link Connection}
      */
     public void setMaxPendingBytesPerConnection(final int maxPendingBytes) {
