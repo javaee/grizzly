3f3aded780539286b5cae5005373b3d295f57db6
oleksiys <oleksiys@04fdb32c-e91d-0410-84f5-98e489d73e21>
2011-01-21 10:03:15 +0000

    + set runners count equal to available processors
    git-svn-id: svn+ssh://svn.java.net/grizzly~svn/branches/2dot0/code@5724 04fdb32c-e91d-0410-84f5-98e489d73e21

--- modules/grizzly/src/main/java/org/glassfish/grizzly/NIOTransportBuilder.java
+++ modules/grizzly/src/main/java/org/glassfish/grizzly/NIOTransportBuilder.java
@@ -419,7 +419,7 @@ public abstract class NIOTransportBuilder<T extends NIOTransportBuilder> {
      *  that should be used.
      */
     private int getRunnerCount() {
-        return Math.max(1, Runtime.getRuntime().availableProcessors() / 2 * 3);
+        return Math.max(1, Runtime.getRuntime().availableProcessors());
     }
 
 }
