dccaa064092055017c7a44931c564ac9e9f6e9c3
cheeser <cheeser@04fdb32c-e91d-0410-84f5-98e489d73e21>
2011-01-21 21:31:34 +0000

    tweak script
    git-svn-id: svn+ssh://svn.java.net/grizzly~svn/branches/2dot0/code@5734 04fdb32c-e91d-0410-84f5-98e489d73e21

--- copyright.sh
+++ copyright.sh
@@ -72,7 +72,7 @@ valid() {
 }
 
 run() {
-	$JAVA -jar $0/../copyright.jar  $* | while read LINE
+	$JAVA -jar copyright.jar  $* | while read LINE
 	do
 		valid $LINE
 	done
@@ -84,7 +84,7 @@ then
 	JAVA=java
 fi
 
-$JAVA -jar $0/../copyright.jar -V
+$JAVA -jar copyright.jar -V
 rm -f copyrightcheck.out
 run $* | tee copyrightcheck.out
 
