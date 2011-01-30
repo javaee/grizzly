d70b1a1a1ec6577e49441eee65f9286616d298d6
rlubke <rlubke@04fdb32c-e91d-0410-84f5-98e489d73e21>
2011-01-30 22:42:46 +0000

    tabs -> spaces
    git-svn-id: svn+ssh://svn.java.net/grizzly~svn/branches/2dot0/code@5788 04fdb32c-e91d-0410-84f5-98e489d73e21

--- modules/http/src/main/java/org/glassfish/grizzly/http/util/HttpStatus.java
+++ modules/http/src/main/java/org/glassfish/grizzly/http/util/HttpStatus.java
@@ -175,30 +175,30 @@ public class HttpStatus {
      */
     public static String filter(String message) {
 
-	if (message == null)
-	    return (null);
+        if (message == null)
+            return (null);
 
-	char content[] = new char[message.length()];
-	message.getChars(0, message.length(), content, 0);
-	StringBuilder result = new StringBuilder(content.length + 50);
-	for (int i = 0; i < content.length; i++) {
-	    switch (content[i]) {
-	    case '<':
-		result.append("&lt;");
-		break;
-	    case '>':
-		result.append("&gt;");
-		break;
-	    case '&':
-		result.append("&amp;");
-		break;
-	    case '"':
-		result.append("&quot;");
-		break;
-	    default:
-		result.append(content[i]);
-	    }
-	}
-	return (result.toString());
+        char content[] = new char[message.length()];
+        message.getChars(0, message.length(), content, 0);
+        StringBuilder result = new StringBuilder(content.length + 50);
+        for (int i = 0; i < content.length; i++) {
+            switch (content[i]) {
+                case '<':
+                    result.append("&lt;");
+                    break;
+                case '>':
+                    result.append("&gt;");
+                    break;
+                case '&':
+                    result.append("&amp;");
+                    break;
+                case '"':
+                    result.append("&quot;");
+                    break;
+                default:
+                    result.append(content[i]);
+            }
+        }
+        return (result.toString());
     }
 }
