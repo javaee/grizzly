efd856bb9219e6d7bec1c01dc308c644eb0581a6
Ryan Lubke <ryan.lubke@oracle.com>
2011-04-21 15:43:51 -0700

    Add missing changes.

--- release.sh
+++ release.sh
@@ -67,10 +67,10 @@ parse() {
 				DEV_VER=${ARGS[1]}
 				;;
 			"user")
-				SVN_USER=${ARGS[1]}
+				JN_USER=${ARGS[1]}
 				;;
 			"password")
-				SVN_PWD=${ARGS[1]}
+				JN_PWD=${ARGS[1]}
 				;;
 			"prepare")
 				PREPARE="release:clean release:prepare"
@@ -92,24 +92,19 @@ parse $*
 verify BRANCH "Missing --tag option (e.g., 1_2_3)"
 verify RELEASE_VER "Missing --release_ver option (e.g., 1.2.3)"
 verify DEV_VER "Missing --dev_ver option (e.g., 1.2.4-SNAPSHOT)"
-verify SVN_USER "Missing --user option for your svn user name"
-verify SVN_PWD "Missing --password option for your svn password"
+verify JN_USER "Missing --user option for your svn user name"
+verify JN_PWD "Missing --password option for your svn password"
 [ "${MISSING}" ] && exit
-
-if [ -z "${PREPARE}" ] 
-then
-	SVN_URL="-DconnectionUrl=scm:svn:https://svn.java.net/svn/grizzly~svn/tags/${BRANCH}/"
-fi
 
 if [ "${PREPARE}" ]
 then
-	CMD="mvn -Darguments=-Dmaven.test.skip.exec=true -e -P release-profile -DdryRun=$DRYRUN -DautoVersionSubmodules=true -DdevelopmentVersion=${DEV_VER} -DreleaseVersion=${RELEASE_VER} -Dtag=${BRANCH} -Dpassword=${SVN_PWD} -Dusername=${SVN_USER} ${PREPARE}"
+	CMD="mvn -Darguments=-Dmaven.test.skip.exec=true -e -P release-profile -DdryRun=$DRYRUN -DautoVersionSubmodules=true -DdevelopmentVersion=${DEV_VER} -DreleaseVersion=${RELEASE_VER} -Dtag=${BRANCH} -Dpassword=${JN_PWD} -Dusername=${JN_USER} ${PREPARE}"
 
 	echo ${CMD}
 	eval ${CMD}
 fi
 
-CMD="mvn -Darguments=-Dmaven.test.skip.exec=true -e -P release-profile -DdryRun=$DRYRUN -DautoVersionSubmodules=true -DdevelopmentVersion=${DEV_VER} -DreleaseVersion=${RELEASE_VER} -Dtag=${BRANCH} -Dpassword=${SVN_PWD} -Dusername=${SVN_USER} ${SVN_URL} release:perform"
+CMD="mvn -Darguments=-Dmaven.test.skip.exec=true -e -P release-profile -DdryRun=$DRYRUN -DautoVersionSubmodules=true -DdevelopmentVersion=${DEV_VER} -DreleaseVersion=${RELEASE_VER} -Dtag=${BRANCH} -Dpassword=${JN_PWD} -Dusername=${JN_USER} ${SVN_URL} release:perform"
 
 echo ${CMD}
 eval ${CMD}
