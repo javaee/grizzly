3515f16d91bd6f5c832263352a8978924c36bfb7
Oleksiy Stashok <oleksiys@mytec.(none)>
2011-07-22 10:55:30 +0200

    + add non-blocking upload sample

---
+++ samples/http-server-samples/src/main/java/org/glassfish/grizzly/samples/httpserver/nonblockinghandler/UploadHttpHandlerSample.java
@@ -0,0 +1,179 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright (c) 2010-2011 Oracle and/or its affiliates. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License.  You can
+ * obtain a copy of the License at
+ * https://glassfish.dev.java.net/public/CDDL+GPL_1_1.html
+ * or packager/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at packager/legal/LICENSE.txt.
+ *
+ * GPL Classpath Exception:
+ * Oracle designates this particular file as subject to the "Classpath"
+ * exception as provided by Oracle in the GPL Version 2 section of the License
+ * file that accompanied this code.
+ *
+ * Modifications:
+ * If applicable, add the following below the License Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyright [year] [name of copyright owner]"
+ *
+ * Contributor(s):
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ */
+package org.glassfish.grizzly.samples.httpserver.nonblockinghandler;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.glassfish.grizzly.Buffer;
+import org.glassfish.grizzly.Grizzly;
+import org.glassfish.grizzly.http.server.HttpHandler;
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.grizzly.http.server.Request;
+import org.glassfish.grizzly.http.server.Response;
+import org.glassfish.grizzly.http.server.ServerConfiguration;
+import org.glassfish.grizzly.http.server.io.NIOInputStream;
+import org.glassfish.grizzly.http.server.io.ReadHandler;
+import org.glassfish.grizzly.http.util.HttpStatus;
+
+/**
+ * The sample shows how the HttpHandler should be implemented in order to
+ * store uploaded data into file in non-blocking way.
+ * 
+ * @author Alexey Stashok
+ */
+public class UploadHttpHandlerSample {
+    private static final Logger LOGGER = Grizzly.logger(UploadHttpHandlerSample.class);
+
+
+    public static void main(String[] args) {
+
+        // create a basic server that listens on port 8080.
+        final HttpServer server = HttpServer.createSimpleServer();
+
+        final ServerConfiguration config = server.getServerConfiguration();
+
+        // Map the path, /upload, to the NonBlockingUploadHandler
+        config.addHttpHandler(new NonBlockingUploadHandler(), "/upload");
+
+        try {
+            server.start();
+            LOGGER.info("Press enter to stop the server...");
+            System.in.read();
+        } catch (IOException ioe) {
+            LOGGER.log(Level.SEVERE, ioe.toString(), ioe);
+        } finally {
+            server.stop();
+        }
+    }
+
+    /**
+     * This handler using non-blocking streams to read POST data and store it
+     * to the local file.
+     */
+    private static class NonBlockingUploadHandler extends HttpHandler {
+        
+        private final AtomicInteger counter = new AtomicInteger();
+
+        // -------------------------------------------- Methods from HttpHandler
+
+
+        @Override
+        public void service(final Request request,
+                            final Response response) throws Exception {
+
+            final NIOInputStream in = request.getInputStream(false); // false argument puts the stream in non-blocking mode
+            
+            final FileChannel fileChannel = new FileOutputStream(
+                    "./" + counter.incrementAndGet() + ".upload").getChannel();
+            
+            response.suspend();  // !!! suspend the Request
+
+            // If we don't have more data to read - onAllDataRead() will be called
+            in.notifyAvailable(new ReadHandler() {
+
+                @Override
+                public void onDataAvailable() throws Exception {
+                    LOGGER.log(Level.INFO, "[onDataAvailable] length: {0}", in.readyData());
+                    storeAvailableData(in, fileChannel);
+                    in.notifyAvailable(this);
+                }
+
+                @Override
+                public void onError(Throwable t) {
+                    LOGGER.log(Level.INFO, "[onError]{0}", t);
+                    response.setStatus(500, t.getMessage());
+                    complete(true);
+                }
+
+                @Override
+                public void onAllDataRead() throws Exception {
+                    LOGGER.log(Level.INFO, "[onAllDataRead] length: {0}", in.readyData());
+                    storeAvailableData(in, fileChannel);
+                    response.setStatus(HttpStatus.ACCEPTED_202);
+                    complete(false);
+                }
+                
+                private void complete(final boolean isError) {
+                    try {
+                        fileChannel.close();
+                    } catch (IOException e) {
+                        if (!isError) {
+                            response.setStatus(500, e.getMessage());
+                        }
+                    }
+                    
+                    try {
+                        in.close();
+                    } catch (IOException e) {
+                        if (!isError) {
+                            response.setStatus(500, e.getMessage());
+                        }
+                    }
+                    
+                    response.resume();
+                }
+            });
+
+        }
+
+        private static void storeAvailableData(NIOInputStream in, FileChannel fileChannel)
+                throws IOException {
+            final Buffer buffer = in.getBuffer();
+            final ByteBuffer byteBuffer = buffer.toByteBuffer();
+            final int oldPos = byteBuffer.position();
+            
+            int written = 0;
+            try {
+                while(byteBuffer.hasRemaining()) {
+                    written += fileChannel.write(byteBuffer);
+                }
+            } finally {
+                byteBuffer.position(oldPos);
+                in.skip(written);
+            }
+        }
+
+    } // END NonBlockingUploadHandler    
+}
