76c4c6aea420c58bd82693e373a279979ea88b75
Ryan Lubke <ryan.lubke@oracle.com>
2011-09-07 11:03:45 -0700

    Copyright updates.

--- modules/http-servlet/src/test/java/org/glassfish/grizzly/servlet/LoadOnStartupTest.java
+++ modules/http-servlet/src/test/java/org/glassfish/grizzly/servlet/LoadOnStartupTest.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2010 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2011 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -40,15 +40,11 @@
 
 package org.glassfish.grizzly.servlet;
 
-import org.glassfish.grizzly.servlet.ver30.*;
-import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Iterator;
 import java.util.LinkedList;
-import java.util.Set;
 
 import static org.junit.Assert.*;
 
