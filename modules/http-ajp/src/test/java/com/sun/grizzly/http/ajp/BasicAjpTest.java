530aa2095c6403d2c6f67833f1af1aa0351bc027
oleksiys <oleksiys@mytec>
2011-11-15 19:06:16 +0100

    + AJP configuration related changes (change unittest)

--- modules/http-ajp/src/test/java/com/sun/grizzly/http/ajp/BasicAjpTest.java
+++ modules/http-ajp/src/test/java/com/sun/grizzly/http/ajp/BasicAjpTest.java
@@ -191,6 +191,7 @@ public class BasicAjpTest extends AjpTestBase {
         };
 
         configureHttpServer(new StaticResourcesAdapter("src/test/resources"));
+        selectorThread.getAjpConfiguration().setShutdownEnabled(true);
         selectorThread.getAjpConfiguration().getShutdownHandlers().add(shutDownHandler);
         
         final byte[] request = new byte[] {0x12, 0x34, 0, 1, AjpConstants.JK_AJP13_SHUTDOWN};
