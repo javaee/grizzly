1799de6756d446acf73fc1b50e15eb106f0e5ce8
oleksiys <oleksiys@mytec>
2011-12-19 22:19:25 +0100

    + fix copyright headers

--- modules/grizzly/src/main/java/org/glassfish/grizzly/utils/SilentConnectionFilter.java
+++ modules/grizzly/src/main/java/org/glassfish/grizzly/utils/SilentConnectionFilter.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2009-2010 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2009-2011 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -48,7 +48,6 @@ import org.glassfish.grizzly.filterchain.FilterChainContext;
 import org.glassfish.grizzly.filterchain.NextAction;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
