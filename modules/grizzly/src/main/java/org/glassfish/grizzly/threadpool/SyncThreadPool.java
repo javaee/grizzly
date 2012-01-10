010b8e9919b38d750e07bc4c7899f430a889f71d
oleksiys <oleksiys@mytec>
2012-01-10 17:05:02 +0100

    + fix issue #1173 http://java.net/jira/browse/GRIZZLY-1173
    "sync thread pool is getting overflowed"

--- modules/grizzly/src/main/java/org/glassfish/grizzly/threadpool/SyncThreadPool.java
+++ modules/grizzly/src/main/java/org/glassfish/grizzly/threadpool/SyncThreadPool.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2010-2011 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2010-2012 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -158,19 +158,8 @@ public class SyncThreadPool extends AbstractThreadPool {
     @Override
     protected void startWorker(Worker worker) {
         super.startWorker(worker);
+        activeThreadsCount++;
         currentPoolSize++;
-    }
-
-    @Override
-    protected void onWorkerStarted(Worker worker) {
-        super.onWorkerStarted(worker);
-
-        stateLock.lock();
-        try {
-            activeThreadsCount++;
-        } finally {
-            stateLock.unlock();
-        }
     }
 
     @Override
