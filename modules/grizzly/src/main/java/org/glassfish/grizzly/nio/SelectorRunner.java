9cf1ebdff0e96b1ad0df073cd8eac88533bb8785
oleksiys <oleksiys@mytec>
2012-02-07 15:41:18 +0100

    + fix issue #1194 http://java.net/jira/browse/GRIZZLY-1194
    "False-positive result of selector empty-spin checker"

--- modules/grizzly/src/main/java/org/glassfish/grizzly/nio/SelectorRunner.java
+++ modules/grizzly/src/main/java/org/glassfish/grizzly/nio/SelectorRunner.java
@@ -485,10 +485,18 @@ public final class SelectorRunner implements Runnable {
         final Selector newSelector = Selectors.newSelector(transport.getSelectorProvider());
 
         final Set<SelectionKey> keys = oldSelector.keys();
+        final SelectionKeyHandler selectionKeyHandler =
+                transport.getSelectionKeyHandler();
+        
         for (final SelectionKey selectionKey : keys) {
             try {
-                selectionKey.channel().register(newSelector,
+                final NIOConnection nioConnection =
+                        selectionKeyHandler.getConnectionForKey(selectionKey);
+                final SelectionKey newSelectionKey =
+                        selectionKey.channel().register(newSelector,
                         selectionKey.interestOps(), selectionKey.attachment());
+                
+                nioConnection.onSelectionKeyUpdated(newSelectionKey);
             } catch (Exception e) {
                 logger.log(Level.FINE, "Error switching channel to a new selector", e);
             }
