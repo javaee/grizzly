e7ea550c03fe0d72ddbc8de7ff52425b523ba7ae
oleksiys <oleksiys@mytec>
2012-03-08 17:35:19 +0100

    + unit test for
    fix issue #1214
    http://java.net/jira/browse/GRIZZLY-1214
    
    "StaticHttpHandler uses non-blocking OutputStream, but doesn't check the max-async-write-queue-size"

---
+++ modules/http-server/src/test/java/org/glassfish/grizzly/http/server/StaticHttpHandlerTest.java
@@ -0,0 +1,309 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright (c) 2012 Oracle and/or its affiliates. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License.  You can
+ * obtain a copy of the License at
+ * https://glassfish.dev.java.net/public/CDDL+GPL_1_1.html
+ * or packager/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at packager/legal/LICENSE.txt.
+ *
+ * GPL Classpath Exception:
+ * Oracle designates this particular file as subject to the "Classpath"
+ * exception as provided by Oracle in the GPL Version 2 section of the License
+ * file that accompanied this code.
+ *
+ * Modifications:
+ * If applicable, add the following below the License Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyright [year] [name of copyright owner]"
+ *
+ * Contributor(s):
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ */
+package org.glassfish.grizzly.http.server;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.net.URL;
+import java.nio.channels.FileChannel;
+import java.security.MessageDigest;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.glassfish.grizzly.Connection;
+import org.glassfish.grizzly.Grizzly;
+import org.glassfish.grizzly.filterchain.*;
+import org.glassfish.grizzly.http.*;
+import org.glassfish.grizzly.http.util.Header;
+import org.glassfish.grizzly.impl.FutureImpl;
+import org.glassfish.grizzly.nio.transport.TCPNIOTransport;
+import org.glassfish.grizzly.nio.transport.TCPNIOTransportBuilder;
+import org.glassfish.grizzly.ssl.SSLContextConfigurator;
+import org.glassfish.grizzly.ssl.SSLEngineConfigurator;
+import org.glassfish.grizzly.ssl.SSLFilter;
+import org.glassfish.grizzly.utils.DelayFilter;
+import org.glassfish.grizzly.utils.Futures;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import static org.junit.Assert.*;
+
+/**
+ * {@link StaticHttpHandler} test.
+ * 
+ * @author Alexey Stashok
+ */
+@RunWith(Parameterized.class)
+public class StaticHttpHandlerTest {
+    private static final int PORT = 18900;
+    private static final Logger LOGGER = Grizzly.logger(StaticHttpHandlerTest.class);
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> getMode() {
+        return Arrays.asList(new Object[][]{
+                    {Boolean.FALSE, Boolean.FALSE},
+                    {Boolean.FALSE, Boolean.TRUE},
+                    {Boolean.TRUE, Boolean.FALSE},
+                    {Boolean.TRUE, Boolean.TRUE},
+                });
+    }
+
+    private HttpServer httpServer;
+    
+    final boolean isSslEnabled;
+    final boolean isFileSendEnabled;
+    
+    public StaticHttpHandlerTest(boolean isFileSendEnabled,
+            boolean isSslEnabled) {
+        this.isFileSendEnabled = isFileSendEnabled;
+        this.isSslEnabled = isSslEnabled;
+    }
+
+    @Before
+    public void before() throws Exception {
+        httpServer = createServer(isFileSendEnabled, isSslEnabled);
+        httpServer.start();
+    }
+
+    @After
+    public void after() throws Exception {
+        if (httpServer != null) {
+            httpServer.stop();
+        }
+    }
+    
+    @Test
+    public void testSlowClient() throws Exception {
+        final int fileSize = 16 * 1024 * 1024;
+        File control = generateTempFile(fileSize);
+        
+        final FutureImpl<File> result = Futures.<File>createSafeFuture();
+
+        TCPNIOTransport client = createClient(result, new ResponseValidator() {
+            @Override
+            public void validate(HttpResponsePacket response) {
+                assertEquals(Integer.toString(fileSize), response.getHeader(Header.ContentLength));
+                // static resource handler won't know how to handle .tmp extension,
+                // so it should punt.
+                assertEquals("text/plain", response.getHeader(Header.ContentType));
+            }
+        }, isSslEnabled);
+        BigInteger controlSum = getMDSum(control);
+        try {
+            client.start();
+            Connection c = client.connect("localhost", PORT).get(10, TimeUnit.SECONDS);
+            
+            HttpRequestPacket request =
+                    HttpRequestPacket.builder().uri("/" + control.getName())
+                        .method(Method.GET)
+                        .protocol(Protocol.HTTP_1_1)
+                        .header("Host", "localhost:" + PORT).build();
+            c.write(request);
+            File fResult = result.get(20, TimeUnit.SECONDS);
+            BigInteger resultSum = getMDSum(fResult);
+            assertTrue("MD5Sum between control and test files differ.",
+                        controlSum.equals(resultSum));
+            
+            c.close();
+        } finally {
+            client.stop();
+        }        
+    }
+    
+    private static TCPNIOTransport createClient(final FutureImpl<File> result,
+            final ResponseValidator validator,
+            final boolean isSslEnabled) throws Exception {
+        TCPNIOTransport transport = TCPNIOTransportBuilder.newInstance().build();
+        FilterChainBuilder builder = FilterChainBuilder.stateless();
+        builder.add(new TransportFilter());
+        
+        // simulate slow read
+        builder.add(new DelayFilter(5, 0));
+        
+        if (isSslEnabled) {
+            final SSLFilter sslFilter = new SSLFilter(createSSLConfig(true),
+                    createSSLConfig(false));
+            builder.add(sslFilter);
+        }
+        
+        builder.add(new HttpClientFilter());
+        builder.add(new BaseFilter() {
+            FileChannel out;
+            File f;
+            long start;
+            long stop;
+
+            @Override
+            public NextAction handleRead(FilterChainContext ctx) throws IOException {
+                HttpContent content = ctx.getMessage();
+                out.write(content.getContent().toByteBuffer());
+                if (content.isLast()) {
+                    stop = System.currentTimeMillis();
+                    if (validator != null) {
+                        validator.validate((HttpResponsePacket) content.getHttpHeader());
+                    }
+                    try {
+                        out.close();
+                        LOGGER.log(Level.INFO, "Client received file ({0} bytes) in {1}ms.",
+                                new Object[]{f.length(), stop - start});
+                        // result.result(f) should be the last operation in handleRead
+                        // otherwise NPE may occur in handleWrite asynchronously
+                        result.result(f);
+                    } catch (IOException ioe) {
+                        ioe.printStackTrace();
+                    }
+                }
+                return ctx.getStopAction();
+            }
+
+            @Override
+            public NextAction handleWrite(FilterChainContext ctx) throws IOException {
+                try {
+                    if (f != null) {
+                        if (!f.delete()) {
+                            LOGGER.log(Level.WARNING, "Unable to explicitly delete file: {0}", f.getAbsolutePath());
+                        }
+                        f = null;
+                    }
+                    f = File.createTempFile("grizzly-test-", ".tmp");
+                    f.deleteOnExit();
+                    out = new FileOutputStream(f).getChannel();
+                } catch (IOException ioe) {
+                    throw new RuntimeException(ioe);
+                }
+                start = System.currentTimeMillis();
+                return super.handleWrite(ctx);
+            }
+        });
+        
+        transport.setProcessor(builder.build());
+        return transport;
+    }
+    
+    private static HttpServer createServer(
+            boolean isFileSendEnabled, boolean isSslEnabled) throws Exception {
+        final HttpServer server = new HttpServer();
+        final NetworkListener listener = 
+                new NetworkListener("test", 
+                                    NetworkListener.DEFAULT_NETWORK_HOST, 
+                                    PORT);
+        
+        if (isSslEnabled) {
+            listener.setSecure(true);
+            listener.setSSLEngineConfig(createSSLConfig(true));
+        }
+        
+        listener.setSendFileEnabled(isFileSendEnabled);
+        server.addListener(listener);
+        server.getServerConfiguration().addHttpHandler(
+                new StaticHttpHandler(System.getProperty("java.io.tmpdir")), "/");
+        
+        return server;
+    }
+
+    private static BigInteger getMDSum(final File f) throws Exception {
+        MessageDigest digest = MessageDigest.getInstance("MD5");
+        byte[] b = new byte[8192];
+        FileInputStream in = new FileInputStream(f);
+        int len;
+        while ((len = in.read(b)) != -1) {
+            digest.update(b, 0, len);
+        }
+        return new BigInteger(digest.digest());
+    }
+
+
+    private static File generateTempFile(final int size) throws IOException {
+        final File f = File.createTempFile("grizzly-temp-" + size, ".tmp");
+        Random r = new Random();
+        byte[] data = new byte[8192];
+        r.nextBytes(data);
+        FileOutputStream out = new FileOutputStream(f);
+        int total = 0;
+        int remaining = size;
+        while (total < size) {
+            int len = ((remaining > 8192) ? 8192 : remaining);
+            out.write(data, 0, len);
+            total += len;
+            remaining -= len;
+        }
+        f.deleteOnExit();
+        return f;
+    }
+    
+    private static SSLEngineConfigurator createSSLConfig(boolean isServer) throws Exception {
+        final SSLContextConfigurator sslContextConfigurator =
+                new SSLContextConfigurator();
+        final ClassLoader cl = SuspendTest.class.getClassLoader();
+        // override system properties
+        final URL cacertsUrl = cl.getResource("ssltest-cacerts.jks");
+        if (cacertsUrl != null) {
+            sslContextConfigurator.setTrustStoreFile(cacertsUrl.getFile());
+            sslContextConfigurator.setTrustStorePass("changeit");
+        }
+
+        // override system properties
+        final URL keystoreUrl = cl.getResource("ssltest-keystore.jks");
+        if (keystoreUrl != null) {
+            sslContextConfigurator.setKeyStoreFile(keystoreUrl.getFile());
+            sslContextConfigurator.setKeyStorePass("changeit");
+        }
+
+        return new SSLEngineConfigurator(sslContextConfigurator.createSSLContext(),
+                !isServer, false, false);
+    }
+    
+    // ---------------------------------------------------------- Nested Classes
+    
+    
+    private static interface ResponseValidator {
+        
+        void validate(HttpResponsePacket response);
+        
+    }    
+}
