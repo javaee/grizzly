4b806a4eb26d23d39c90bcb650ad1450f4ec28c4
Ryan Lubke <ryan.lubke@oracle.com>
2012-03-12 10:38:26 -0700

    Fixes for http://java.net/jira/browse/GRIZZLY-1218 and http://java.net/jira/browse/GRIZZLY-1220.

--- modules/http-servlet/src/main/java/org/glassfish/grizzly/servlet/FilterChainImpl.java
+++ modules/http-servlet/src/main/java/org/glassfish/grizzly/servlet/FilterChainImpl.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2011 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2011-2012 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -196,7 +196,7 @@ final class FilterChainImpl implements FilterChain, FilterChainInvoker {
         for (int i = 0, len = listeners.length; i < len; i++) {
             if (listeners[i] instanceof ServletRequestListener) {
                 try {
-                    ((ServletRequestListener) listeners[i]).requestDestroyed(event);
+                    ((ServletRequestListener) listeners[i]).requestInitialized(event);
                 } catch (Throwable t) {
                     if (LOGGER.isLoggable(Level.WARNING)) {
                         LOGGER.log(Level.WARNING,
