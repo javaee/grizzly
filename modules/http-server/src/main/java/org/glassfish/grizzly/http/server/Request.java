96db45acbd921b4aae4e699e6177564b2f4bc857
oleksiys <oleksiys@mytec>
2012-03-16 17:35:59 +0100

    + fix issue #fix issue #1225 (2.2.x) http://java.net/jira/browse/GRIZZLY-1225
    "Invalid function call in Regquest#isRequestedSessionIdValid"
    
    (the fix was suggested by martin_novak)

--- modules/http-server/src/main/java/org/glassfish/grizzly/http/server/Request.java
+++ modules/http-server/src/main/java/org/glassfish/grizzly/http/server/Request.java
@@ -2277,7 +2277,7 @@ public class Request {
             return session.isValid();
         }
 
-        Session localSession = sessions.put(requestedSessionId, session);
+        Session localSession = sessions.get(requestedSessionId);
         return ((localSession != null) && localSession.isValid());
 
     }
