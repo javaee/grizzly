1b0348240f052af62e4ce599566b764d26fe8601
oleksiys <oleksiys@mytec>
2012-03-21 16:05:20 +0100

    + fix issue #fix issue #1232 (2.2.x) http://java.net/jira/browse/GRIZZLY-1232
    "Max queued data limit exceeded" while streaming large data using webservice"

--- modules/http-servlet/src/main/java/org/glassfish/grizzly/servlet/HttpServletResponseImpl.java
+++ modules/http-servlet/src/main/java/org/glassfish/grizzly/servlet/HttpServletResponseImpl.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2008-2011 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2008-2012 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -153,6 +153,7 @@ public class HttpServletResponseImpl implements HttpServletResponse {
 
     public void initialize(final Response response) throws IOException {
         this.response = response;
+        response.getOutputBuffer().setAsyncEnabled(false); // switch Grizzly output to blocking mode by default
         outputStream.initialize(response);
 
     }
