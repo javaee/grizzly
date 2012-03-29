d7203c44e8be4469a7a891786077d84fa2f16679
oleksiys <oleksiys@mytec>
2012-03-29 15:57:16 +0200

    + unit tests for issue #1213 http://java.net/jira/browse/GRIZZLY-1213 "JK listener with Apache + mod_ajp_proxy causes truncated downloads"

--- modules/http-ajp/src/test/java/com/sun/grizzly/http/ajp/AjpTestBase.java
+++ modules/http-ajp/src/test/java/com/sun/grizzly/http/ajp/AjpTestBase.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2010-2011 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2010-2012 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -147,6 +147,7 @@ public class AjpTestBase {
         selectorThread.setTcpNoDelay(true);
         selectorThread.setUseChunking(false);
         selectorThread.setKeepAliveTimeoutInSeconds(1);
+        selectorThread.setSendBufferSize(512);
 
         selectorThread.listen();
     }
