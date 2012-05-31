ce08f6c813f4cfe2f08f2e715863ae48f3f16d94
Ryan Lubke <ryan.lubke@oracle.com>
2012-05-31 11:57:52 -0700

    [2.2.x] Copyright updates.

--- modules/http/src/main/java/org/glassfish/grizzly/http/util/MimeHeaders.java
+++ modules/http/src/main/java/org/glassfish/grizzly/http/util/MimeHeaders.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2010-2011 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2010-2012 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -59,8 +59,6 @@
 package org.glassfish.grizzly.http.util;
 
 import org.glassfish.grizzly.Buffer;
-import org.glassfish.grizzly.memory.Buffers;
-import org.glassfish.grizzly.memory.MemoryManager;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
