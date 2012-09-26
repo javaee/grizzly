a41bd1207174e048bf3e490f86f2f63d02f22a52
oleksiys <oleksiys@baterka>
2012-09-26 16:42:38 +0200

    + [1.9.x] fix for the issue #1339 http://java.net/jira/browse/GRIZZLY-1339 "What causes GRIZZLY to flood the Glassfish-Log with messages like this 'Interrupting idle Thread: http-thread-pool-80(3)'"

--- modules/http/src/main/java/com/sun/grizzly/http/KeepAliveThreadAttachment.java
+++ modules/http/src/main/java/com/sun/grizzly/http/KeepAliveThreadAttachment.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2009-2011 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2009-2012 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -134,6 +134,7 @@ public class KeepAliveThreadAttachment extends ThreadAttachment{
             }
             
             setIdleTimeoutDelay(UNLIMITED_TIMEOUT);
+            setTransactionTimeout(UNLIMITED_TIMEOUT);
             t.interrupt();
             return false;
         }
