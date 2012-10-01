a809fb216cb09c37c92e0980f072e84e4526a637
Ryan Lubke <ryan.lubke@oracle.com>
2012-10-01 14:16:08 -0700

    Cannot determine if null would ever be passed by HK2, so best to guard against it.

--- modules/config/src/main/java/org/glassfish/grizzly/config/dom/NetworkAddressValidator.java
+++ modules/config/src/main/java/org/glassfish/grizzly/config/dom/NetworkAddressValidator.java
@@ -50,6 +50,9 @@ public class NetworkAddressValidator implements ConstraintValidator<NetworkAddre
     }
 
     public boolean isValid(final String s, final ConstraintValidatorContext constraintValidatorContext) {
+        if (s == null || s.length() == 0) {
+            return false;
+        }
         if (s.charAt(0) == '$'  && s.charAt(1) == '{' && s.charAt(s.length() - 1) == '}') {
             return true;
         }
