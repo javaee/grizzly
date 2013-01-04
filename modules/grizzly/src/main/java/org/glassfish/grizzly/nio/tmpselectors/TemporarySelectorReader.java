5c18734d1af76c7a6b852fbd96ca3e5093659243
Ryan Lubke <ryan.lubke@oracle.com>
2013-01-04 14:53:25 -0800

    [2.3.x] Leverage existing sync handshake code.

--- modules/grizzly/src/main/java/org/glassfish/grizzly/nio/tmpselectors/TemporarySelectorReader.java
+++ modules/grizzly/src/main/java/org/glassfish/grizzly/nio/tmpselectors/TemporarySelectorReader.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2008-2011 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2008-2013 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -165,7 +165,8 @@ public abstract class TemporarySelectorReader
         Selector readSelector = null;
         SelectionKey key = null;
         final SelectableChannel channel = connection.getChannel();
-        final long readTimeout = TimeUnit.MILLISECONDS.convert(timeout, timeunit);
+        final long readTimeout = TimeUnit.MILLISECONDS.convert(
+                ((timeout < 0) ? 0 : timeout), timeunit);
 
         try {
             bytesRead = readNow0(connection, buffer, currentResult);
