069584ab488597b5c70b0146c4f72f3534e213a6
Ryan Lubke <ryan.lubke@oracle.com>
2013-01-15 11:44:11 -0800

    [2.2.x] Fix for http://java.net/jira/browse/GRIZZLY-1399 (NPE when enabling FINE logging for org.glassfish.grizzly.config).

--- modules/config/src/main/java/org/glassfish/grizzly/config/SSLConfigurator.java
+++ modules/config/src/main/java/org/glassfish/grizzly/config/SSLConfigurator.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2009-2012 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2009-2013 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -196,7 +196,7 @@ public class SSLConfigurator extends SSLEngineConfigurator {
             if (LOGGER.isLoggable(Level.FINE)) {
                 LOGGER.log(Level.FINE, "Enabled secure protocols={0}"
                         + "" + " ciphers={1}", new Object[]
-                        {Arrays.asList(enabledProtocols), Arrays.asList(enabledCipherSuites)});
+                        {Arrays.toString(enabledProtocols), Arrays.toString(enabledCipherSuites)});
             }
             
             return newSslContext;
