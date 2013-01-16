cd35bcb772e800f23e74f1c9eb829f8900a35811
oleksiys <oleksiys@baterka>
2013-01-16 17:46:13 +0100

    [2.3.x] + fix issue #1409 http://java.net/jira/browse/GRIZZLY-1409 "Change logging level for "broken pipe" or "connection reset by peer" IOException logging"

--- modules/http-server/src/main/java/org/glassfish/grizzly/http/server/StaticHttpHandler.java
+++ modules/http-server/src/main/java/org/glassfish/grizzly/http/server/StaticHttpHandler.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2010-2012 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2010-2013 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -518,7 +518,7 @@ public class StaticHttpHandler extends HttpHandler {
 
         @Override
         public void onError(Throwable t) {
-            LOGGER.log(Level.WARNING, "[onError] ", t);
+            LOGGER.log(Level.FINE, "[onError] ", t);
             response.setStatus(500, t.getMessage());
             complete(true);
         }
