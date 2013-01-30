867b440eec8e0141d5d69561e5fe9295d3983abf
Ryan Lubke <ryan.lubke@oracle.com>
2013-01-30 15:00:08 -0800

    [master] Revert fix for http://java.net/jira/browse/GRIZZLY-1417 (HttpHeader.quotedCharsetValue isn't reset properly leading to responses with content-types like application/octet-stream;charset=UTF-8)

--- modules/http/src/main/java/org/glassfish/grizzly/http/HttpHeader.java
+++ modules/http/src/main/java/org/glassfish/grizzly/http/HttpHeader.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2010-2013 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2010-2012 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -88,7 +88,6 @@ public abstract class HttpHeader extends HttpPacket
      * Has the charset been explicitly set.
      */
     protected boolean charsetSet = false;
-    protected boolean charsetFromContentType;
 
 //    private String defaultContentType;
 
@@ -492,7 +491,9 @@ public abstract class HttpHeader extends HttpPacket
         }
 
         characterEncoding = charset;
+        // START SJSAS 6316254
         quotedCharsetValue = charset;
+        // END SJSAS 6316254
         charsetSet = true;
     }
 
@@ -577,11 +578,9 @@ public abstract class HttpHeader extends HttpPacket
 
         if (ret != null
                 && quotedCharsetValue != null
-                && (charsetSet || charsetFromContentType)) {
+                && charsetSet) {
 
-            final StringBuilder sb =
-                    new StringBuilder(ret.length() + quotedCharsetValue.length() + 9);
-            ret = sb.append(ret).append(";charset=").append(quotedCharsetValue).toString();
+            ret = ret + ";charset=" + quotedCharsetValue;
         }
 
         return ret;
@@ -600,10 +599,6 @@ public abstract class HttpHeader extends HttpPacket
 
         if (type == null) {
             contentType = null;
-            if (charsetFromContentType) {
-                charsetFromContentType = false;
-                quotedCharsetValue = null;
-            }
             return;
         }
 
@@ -640,10 +635,6 @@ public abstract class HttpHeader extends HttpPacket
 
         if (!hasCharset) {
             contentType = type;
-            if (charsetFromContentType) {
-                charsetFromContentType = false;
-                quotedCharsetValue = null;
-            }
             return;
         }
 
@@ -660,7 +651,7 @@ public abstract class HttpHeader extends HttpPacket
 
         // The charset value may be quoted, but must not contain any quotes.
         if (charsetValue != null && charsetValue.length() > 0) {
-            charsetFromContentType = true;
+            charsetSet=true;
             // START SJSAS 6316254
             quotedCharsetValue = charsetValue;
             // END SJSAS 6316254
@@ -894,7 +885,6 @@ public abstract class HttpHeader extends HttpPacket
 //        defaultContentType = null;
         quotedCharsetValue = null;
         charsetSet = false;
-        charsetFromContentType = false;
         contentType = null;
         chunkingAllowed = false;
         transferEncoding = null;
