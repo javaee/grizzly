92c74d61c7cd187e5fde2ef06e94d9bbddebd1ac
Ryan Lubke <ryan.lubke@oracle.com>
2013-02-26 16:20:59 -0800

    [2.3.x] Fix for http://java.net/jira/browse/GRIZZLY-1418 (Consecutive calls to static data results in loss of Content-Type data on Response Headers)

--- modules/http-servlet/src/main/java/org/glassfish/grizzly/servlet/DefaultServlet.java
+++ modules/http-servlet/src/main/java/org/glassfish/grizzly/servlet/DefaultServlet.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2011 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2011-2013 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -134,8 +134,7 @@ public class DefaultServlet extends HttpServlet {
             resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
         }
 
-        addToFileCache(req, resource);
-        sendFile(resp, resource);
+        sendFile(req, resp, resource);
 
 
     }
@@ -173,7 +172,9 @@ public class DefaultServlet extends HttpServlet {
 
     // --------------------------------------------------------- Private Methods
 
-    public static void sendFile(final HttpServletResponse response, final File file)
+    private void sendFile(final HttpServletRequest request,
+                          final HttpServletResponse response,
+                          final File file)
             throws IOException {
         final String path = file.getPath();
         FileInputStream fis = null;
@@ -210,7 +211,7 @@ public class DefaultServlet extends HttpServlet {
             if (length <= Integer.MAX_VALUE) {
                 response.setContentLength((int) length);
             }
-
+            addToFileCache(request, file);
 
             final OutputStream out = response.getOutputStream();
 
