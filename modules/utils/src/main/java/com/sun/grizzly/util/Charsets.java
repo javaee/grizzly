0caa092eb537f1d047d073d9ba3302e0e75ed9a9
oleksiys <oleksiys@java.net>
2013-03-13 16:32:43 -0700

    + add utility methods to cache Charset encoder/decoder

--- modules/utils/src/main/java/com/sun/grizzly/util/Charsets.java
+++ modules/utils/src/main/java/com/sun/grizzly/util/Charsets.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2011 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2011-2013 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -41,6 +41,8 @@
 package com.sun.grizzly.util;
 
 import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
+import java.nio.charset.CharsetEncoder;
 import java.nio.charset.UnsupportedCharsetException;
 import java.util.Locale;
 import java.util.Map;
@@ -67,6 +69,12 @@ public final class Charsets {
     public static final Charset UTF8_CHARSET = lookupCharset("UTF-8");
     public static final Charset DEFAULT_CHARSET = lookupCharset(DEFAULT_CHARACTER_ENCODING);
 
+    public static final int CODECS_CACHE_SIZE = 2;
+    private static final CharsetCodecResolver DECODER_RESOLVER =
+            new DecoderResolver();
+    private static final CharsetCodecResolver ENCODER_RESOLVER =
+            new EncoderResolver();
+    
     private static volatile boolean areCharsetsPreloaded;
     
     /**
@@ -125,5 +133,136 @@ public final class Charsets {
             areCharsetsPreloaded = false;
             charsetAliasMap.clear();
         }
-    }        
+    }
+    
+    /**
+     * Return the {@link Charset}'s {@link CharsetDecoder}.
+     * The <tt>Charsets</tt> class maintains the {@link CharsetDecoder} thread-local
+     * cache.  Be aware - this shouldn't be used by multiple threads.
+     * 
+     * @param charset {@link Charset}.
+     * @return the {@link Charset}'s {@link CharsetDecoder}.
+     */
+    public static CharsetDecoder getCharsetDecoder(final Charset charset) {
+        if (charset == null) {
+            throw new IllegalArgumentException("Charset can not be null");
+        }
+        
+        final CharsetDecoder decoder = obtainCodecsCache().getDecoder(charset);
+        decoder.reset();
+        
+        return decoder;
+    }
+    
+    /**
+     * Return the {@link Charset}'s {@link CharsetEncoder}.
+     * The <tt>Charsets</tt> class maintains the {@link CharsetEncoder} thread-local
+     * cache.  Be aware - this shouldn't be used by multiple threads.
+     * 
+     * @param charset {@link Charset}.
+     * @return the {@link Charset}'s {@link CharsetEncoder}.
+     */
+    public static CharsetEncoder getCharsetEncoder(final Charset charset) {
+        if (charset == null) {
+            throw new IllegalArgumentException("Charset can not be null");
+        }
+        
+        final CharsetEncoder encoder = obtainCodecsCache().getEncoder(charset);
+        encoder.reset();
+        
+        return encoder;
+    }
+    
+    private final static ThreadLocal<CodecsCache> CODECS_CACHE =
+            new ThreadLocal<CodecsCache>() {
+        @Override
+        protected CodecsCache initialValue() {
+            return new CodecsCache();
+        }
+    };
+    
+    private static CodecsCache obtainCodecsCache() {
+        return CODECS_CACHE.get();
+    }
+
+    private static final class CodecsCache {
+        private final Object[] decoders =
+                new Object[CODECS_CACHE_SIZE];
+        private final Object[] encoders =
+                new Object[CODECS_CACHE_SIZE];
+        
+        public CharsetDecoder getDecoder(final Charset charset) {
+            return (CharsetDecoder) obtainElementByCharset(
+                    decoders, charset, DECODER_RESOLVER);
+        }
+        
+        public CharsetEncoder getEncoder(final Charset charset) {
+            return (CharsetEncoder) obtainElementByCharset(
+                    encoders, charset, ENCODER_RESOLVER);
+        }
+        
+        private static Object obtainElementByCharset(final Object[] array,
+                final Charset charset, final CharsetCodecResolver resolver) {
+            
+            int i = 0;
+            for (; i < array.length; i++) {
+                final Object currentElement = array[i];
+                
+                if (currentElement == null) {
+                    i++; // to make 
+                    break;
+                }
+                
+                if (charset.equals(resolver.charset(currentElement))) {
+                    makeFirst(array, i, currentElement);
+                    return currentElement;
+                }
+            }
+
+            final Object newElement = resolver.newElement(charset);
+            makeFirst(array, i - 1, newElement);
+            return newElement;            
+        }
+        
+        private static void makeFirst(final Object[] array, final int offs,
+                final Object element) {
+            for (int i = offs - 1; i >= 0; i--) {
+                array[i + 1] = array[i];
+            }
+            
+            array[0] = element;
+        }       
+    }
+    
+    private static interface CharsetCodecResolver {
+        Charset charset(Object element);
+        Object newElement(Charset charset);
+    }
+
+    private final static class DecoderResolver implements CharsetCodecResolver {
+        @Override
+        public Charset charset(final Object element) {
+            return ((CharsetDecoder) element).charset();
+        }
+
+        @Override
+        public Object newElement(final Charset charset) {
+            return charset.newDecoder();
+        }
+
+    }
+
+    private final static class EncoderResolver implements CharsetCodecResolver {
+
+        @Override
+        public Charset charset(final Object element) {
+            return ((CharsetEncoder) element).charset();
+        }
+
+        @Override
+        public Object newElement(final Charset charset) {
+            return charset.newEncoder();
+        }
+    }
+    
 }
