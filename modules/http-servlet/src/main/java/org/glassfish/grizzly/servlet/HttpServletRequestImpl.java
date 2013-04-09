93e46f074ef734c98e073b853915a5233b5601bc
Ryan Lubke <ryan.lubke@oracle.com>
2013-04-09 16:11:59 -0700

    [master] Fix compilation error.

--- modules/http-servlet/src/main/java/org/glassfish/grizzly/servlet/HttpServletRequestImpl.java
+++ modules/http-servlet/src/main/java/org/glassfish/grizzly/servlet/HttpServletRequestImpl.java
@@ -204,7 +204,6 @@ public class HttpServletRequestImpl implements HttpServletRequest, Holders.Reque
                            WebappContext context)
             throws IOException {
         this.request = request;
-        request.getInputBuffer().setAsyncEnabled(false); // switch Grizzly input to blocking mode by default
         inputStream.initialize();
         contextImpl = context;
     }
