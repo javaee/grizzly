e9a2429a583e7723709e1225441d1414b5a047b8
Ryan Lubke <ryan.lubke@oracle.com>
2013-06-14 13:11:34 -0700

    [2.3.x] Fix for https://java.net/jira/browse/GRIZZLY-1528 (100-Continue parsing with byte[] within HttpClientFilter sets incorrect start value.)

--- modules/http/src/main/java/org/glassfish/grizzly/http/HttpClientFilter.java
+++ modules/http/src/main/java/org/glassfish/grizzly/http/HttpClientFilter.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2010-2012 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2010-2013 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -368,10 +368,7 @@ public class HttpClientFilter extends HttpCodecFilter {
                         // response from the server after it has sent a
                         // 100-Continue.
                         parsingState.offset += 2;
-                        parsingState.start = 0;
-//                        input.position(parsingState.offset);
-//                        input.shrink();
-//                        parsingState.offset = 0;
+                        parsingState.start = parsingState.offset;
                         return false;
                     }
                     return true;
