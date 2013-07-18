96f31693c10ab6bb7a657322530eae1fae3d996c
oleksiys <oleksiys@java.net>
2013-07-18 13:39:35 -0700

    [glassfishv4] + fix issue #1549 https://java.net/jira/browse/GRIZZLY-1549 "HttpServletRequest.getServerName() returns null for websocket connection initialization GET requests "

--- modules/http/src/main/java/org/glassfish/grizzly/http/HttpServerFilter.java
+++ modules/http/src/main/java/org/glassfish/grizzly/http/HttpServerFilter.java
@@ -639,7 +639,7 @@ public class HttpServerFilter extends HttpCodecFilter {
             request.getProcessingState().error = true;
             return;
         }
-
+        
         final MimeHeaders headers = request.getHeaders();
         
         DataChunk hostDC = null;
