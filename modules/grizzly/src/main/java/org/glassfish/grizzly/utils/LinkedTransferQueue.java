2d0889917e15abb977d68ee4a9a99bc125952767
oleksiys <oleksiys@java.net>
2013-08-20 15:54:07 -0700

    [2.3.x] + leftover after: Implement task #1577 https://java.net/jira/browse/GRIZZLY-1579
    "Removed masked LinkedTransferQueue class"

--- modules/grizzly/src/main/java/org/glassfish/grizzly/utils/LinkedTransferQueue.java
+++ modules/grizzly/src/main/java/org/glassfish/grizzly/utils/LinkedTransferQueue.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2009-2011 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2009-2013 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -50,7 +50,6 @@ import java.util.Collection;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 import java.util.Queue;
-import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.LockSupport;
 
