7f97bc6dbb1ad672499ebebdc6e0d2e85e8623b8
oleksiys <oleksiys@java.net>
2014-02-04 11:22:45 -0800

    [2.3.x] + we don't need this reference

--- modules/http-server/src/main/java/org/glassfish/grizzly/http/server/FileCacheFilter.java
+++ modules/http-server/src/main/java/org/glassfish/grizzly/http/server/FileCacheFilter.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2010-2013 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2010-2014 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -208,10 +208,6 @@ public class FileCacheFilter extends BaseFilter {
                 cacheEntry.getFile(isServeCompressed),
                 0, cacheEntry.getFileSize(isServeCompressed));
         ctx.write(f, new EmptyCompletionHandler<WriteResult>() {
-            // keep strong ref to FileCacheEntry to save it from GC,
-            // because GC may remove the actual file (the compressed one)
-            final FileCacheEntry strongRef = cacheEntry;
-            
             @Override
             public void failed(Throwable throwable) {
                 LOGGER.log(Level.FINE, "Error reported during file-send entry: " +
