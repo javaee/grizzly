01441f3e220916a75a4ce41a19c1b76050eeaa11
oleksiys <oleksiys@java.net>
2014-02-18 15:34:34 -0800

    [master] implement improvement #1650 https://java.net/jira/browse/GRIZZLY-1650 "Glassfish AJP connector has fixed max packet size (should be configurable)"

--- modules/http-ajp/src/main/java/org/glassfish/grizzly/http/ajp/AjpHandlerFilter.java
+++ modules/http-ajp/src/main/java/org/glassfish/grizzly/http/ajp/AjpHandlerFilter.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2010-2013 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2010-2014 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -501,7 +501,7 @@ public class AjpHandlerFilter extends BaseFilter {
             buffer.put((byte) 'B');
             buffer.putShort((short) 3);
             buffer.put(AjpConstants.JK_AJP13_GET_BODY_CHUNK);
-            buffer.putShort((short) AjpConstants.MAX_READ_SIZE);
+            buffer.putShort((short) AjpConstants.SUGGESTED_MAX_PAYLOAD_SIZE);
 
             buffer.flip();
             buffer.allowBufferDispose(true);
