93515b1a5c9778530ea803d986df58d5a48833d2
oleksiys <oleksiys@java.net>
2014-09-02 14:02:09 +0200

    [2.3.x] + (additional) fix issue #1706 https://java.net/jira/browse/GRIZZLY-1706 "Can't create junit test of Filter implementation because FilterChainContext class is final"

--- modules/grizzly/src/main/java/org/glassfish/grizzly/utils/DelayedExecutor.java
+++ modules/grizzly/src/main/java/org/glassfish/grizzly/utils/DelayedExecutor.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2009-2013 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2009-2014 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -177,7 +177,7 @@ public class DelayedExecutor {
         }
     }
 
-    public final class DelayQueue<E> {
+    public class DelayQueue<E> {
         final ConcurrentMap<E, DelayQueue> queue =
                 DataStructures.<E, DelayQueue>getConcurrentMap();
 
