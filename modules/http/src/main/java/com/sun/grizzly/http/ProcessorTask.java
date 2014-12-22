685e0fad1281f02d06dd786c6969a201b41f3565
oleksiys <oleksiys@java.net>
2014-12-22 11:15:16 -0800

    [1.9.x] + fix issue #1721 https://java.net/jira/browse/GRIZZLY-1721 "Sporadic HTTP(S) response corruption due to incorrect objects recycling"

--- modules/http/src/main/java/com/sun/grizzly/http/ProcessorTask.java
+++ modules/http/src/main/java/com/sun/grizzly/http/ProcessorTask.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2007-2013 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2007-2014 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -984,10 +984,6 @@ public class ProcessorTask extends TaskBase implements Processor,
             connectionHeaderValueSet = false;
         }
 
-        if (isProcessingCompleted) {
-            return;
-        }
-        
         inputBuffer.recycle();
         outputBuffer.recycle();
 
