538fde16847a305bb38bc05a55f4a32f94119a27
oleksiys <oleksiys@java.net>
2015-01-27 23:11:57 -0800

    [master] [master] + make ClientHttpFilter skip HTTP 2.0 parsing

--- modules/http/src/main/java/org/glassfish/grizzly/http/HttpCodecFilter.java
+++ modules/http/src/main/java/org/glassfish/grizzly/http/HttpCodecFilter.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2010-2014 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2010-2015 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -474,7 +474,7 @@ public abstract class HttpCodecFilter extends HttpBaseFilter
             final HttpPacketParsing httpPacket) throws IOException {
 
         // Get the input buffer
-        Buffer input = (Buffer) ctx.getMessage();
+        Buffer input = ctx.getMessage();
 
         // Get the Connection
         final Connection connection = ctx.getConnection();
