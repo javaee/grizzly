0cfe70421fae98e92db665aa234a2c18528fe563
oleksiys <oleksiys@java.net>
2015-01-30 18:43:16 -0800

    [master] + minor typo fix

--- samples/http-server-samples/src/main/java/org/glassfish/grizzly/samples/httpserver/secure/Server.java
+++ samples/http-server-samples/src/main/java/org/glassfish/grizzly/samples/httpserver/secure/Server.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2014 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2014-2015 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -55,7 +55,7 @@ import org.glassfish.grizzly.ssl.SSLEngineConfigurator;
  * Secured standalone Java HTTP server.
  */
 public class Server {
-    private static final Logger LOGGER = Grizzly.logger(org.glassfish.grizzly.samples.httpserver.priorities.Server.class);
+    private static final Logger LOGGER = Grizzly.logger(Server.class);
     
     public static void main(String[] args) {
         final HttpServer server = new HttpServer();
