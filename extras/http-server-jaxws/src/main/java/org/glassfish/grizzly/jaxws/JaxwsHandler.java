ea1ef76e095f4e2f0b2c51668d802699b5addc4d
oleksiys <oleksiys@java.net>
2015-02-02 12:25:37 -0800

    [master] + bump jax-ws-ri to 2.2.10, make the sun/oracle jax-ws-rt dependency "provided"

--- extras/http-server-jaxws/src/main/java/org/glassfish/grizzly/jaxws/JaxwsHandler.java
+++ extras/http-server-jaxws/src/main/java/org/glassfish/grizzly/jaxws/JaxwsHandler.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2011-2012 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2011-2015 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -64,7 +64,6 @@ import java.util.logging.Logger;
 import javax.xml.namespace.QName;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.Source;
-import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
 import javax.xml.ws.Endpoint;
 import org.glassfish.grizzly.EmptyCompletionHandler;
