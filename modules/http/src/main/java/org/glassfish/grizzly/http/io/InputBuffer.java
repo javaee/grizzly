d64d33ec5817a79a002a91bd51bf76b839fbbb29
oleksiys <oleksiys@java.net>
2015-05-08 14:10:26 -0700

    [2.3.x] + fix issue #1767 https://java.net/jira/browse/GRIZZLY-1767 "HTTP InputBuffer reset/mark is broken"

--- modules/http/src/main/java/org/glassfish/grizzly/http/io/InputBuffer.java
+++ modules/http/src/main/java/org/glassfish/grizzly/http/io/InputBuffer.java
@@ -322,12 +322,7 @@ public class InputBuffer {
             }
         }
         
-        if (readAheadLimit != -1) {
-            readCount++;
-            if (readCount > readAheadLimit) {
-                markPos = -1;
-            }
-        }
+        checkMarkAfterRead(1);
         return inputContentBuffer.get() & 0xFF;
 
     }
@@ -486,12 +481,8 @@ public class InputBuffer {
             throw new IllegalArgumentException("target cannot be null.");
         }
         final int read = fillChars(1, target);
-        if (readAheadLimit != -1) {
-            readCount += read;
-            if (readCount > readAheadLimit) {
-                markPos = -1;
-            }
-        }
+        checkMarkAfterRead(read);
+        
         return read;
 
     }
@@ -628,7 +619,7 @@ public class InputBuffer {
      * @see java.io.Reader#mark(int)
      */
     public void mark(final int readAheadLimit) {
-
+        
         if (readAheadLimit > 0) {
             markPos = inputContentBuffer.position();
             readCount = 0;
@@ -667,15 +658,11 @@ public class InputBuffer {
             throw new IOException();
         }
 
-        if (readAheadLimit == -1 && markPos == -1) {
+        if (readAheadLimit == -1) {
             throw new IOException("Mark not set");
         }
-        if (readAheadLimit != -1) {
-            if (markPos == -1) {
-                throw new IOException("Mark not set");
-            }
-            readCount = 0;
-        }
+        
+        readCount = 0;
         inputContentBuffer.position(markPos);
 
     }
@@ -1244,7 +1231,9 @@ public class InputBuffer {
             if (consumedBytes > 0) {
                 bytes.position(bytesPos);
                 inputContentBuffer.position(inputContentBuffer.position() + consumedBytes);
-                inputContentBuffer.shrink();
+                if (readAheadLimit == -1) {
+                    inputContentBuffer.shrink();
+                }
             } else {
                 isNeedMoreInput = true;
             }
@@ -1406,7 +1395,7 @@ public class InputBuffer {
      *      mark hasn't been set or has been invalidated
      */
     private boolean checkMarkAfterRead(final long n) {
-        if (readAheadLimit != -1) {
+        if (n > 0 && readAheadLimit != -1) {
             if ((long) readCount + n <= readAheadLimit) {
                 readCount += n;
                 return true;
