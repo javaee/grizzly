6d73e4c977af0b837e26aa727fc5be0fd913411f
oleksiys <oleksiys@java.net>
2015-05-19 12:53:12 -0700

    [master] + minor fixes

--- modules/spdy/src/test/java/org/glassfish/grizzly/spdy/HttpInputStreamsTest.java
+++ modules/spdy/src/test/java/org/glassfish/grizzly/spdy/HttpInputStreamsTest.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2010-2014 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2010-2015 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -1277,7 +1277,7 @@ public class HttpInputStreamsTest extends AbstractSpdyTest {
             Future<Connection> connectFuture = ctransport.connect("localhost", PORT);
             Connection connection = null;
             try {
-                connection = connectFuture.get(30, TimeUnit.MINUTES);
+                connection = connectFuture.get(30, TimeUnit.SECONDS);
                 testResult.get(60, TimeUnit.SECONDS);
             } finally {
                 // Close the client connection
