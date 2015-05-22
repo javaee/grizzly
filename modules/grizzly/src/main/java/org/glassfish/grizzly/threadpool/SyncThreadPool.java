1b037645e28630905d9cc1028168f61882be1c2d
oleksiys <oleksiys@java.net>
2015-05-21 17:27:49 -0700

    [master] + fix issue #1773 https://java.net/jira/browse/GRIZZLY-1773 "SyncThreadPool keep-alive doesn't release the thread properly"

--- modules/grizzly/src/main/java/org/glassfish/grizzly/threadpool/SyncThreadPool.java
+++ modules/grizzly/src/main/java/org/glassfish/grizzly/threadpool/SyncThreadPool.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2010-2013 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2010-2015 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -212,11 +212,11 @@ public class SyncThreadPool extends AbstractThreadPool {
                         if (!running) {
                             return null;
                         } else if (hasKeepAlive) {
+                            keepAliveMillis = endTime - System.currentTimeMillis();
+                            
                             if (keepAliveMillis < 20) {
                                 return null;
                             }
-
-                            keepAliveMillis = endTime - System.currentTimeMillis();
                         }
                     } while (true);
 
