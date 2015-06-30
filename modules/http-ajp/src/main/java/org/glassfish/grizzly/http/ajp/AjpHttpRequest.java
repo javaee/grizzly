102c432aa2004c581db1c297e94b0eb90dd74122
rlubke <ryan.lubke@oracle.com>
2015-06-30 09:31:10 -0700

    [2.3.x] Changes for https://java.net/jira/browse/GRIZZLY-1787.   - Enable chunking in AJP.

--- modules/http-ajp/src/main/java/org/glassfish/grizzly/http/ajp/AjpHttpRequest.java
+++ modules/http-ajp/src/main/java/org/glassfish/grizzly/http/ajp/AjpHttpRequest.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2011-2013 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2011-2015 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -225,6 +225,7 @@ public final class AjpHttpRequest extends HttpRequestPacket {
     
     private AjpHttpRequest init() {
         cachedResponse.setRequest(this);
+        cachedResponse.setChunkingAllowed(true);
         setResponse(cachedResponse);
         return this;
     }
