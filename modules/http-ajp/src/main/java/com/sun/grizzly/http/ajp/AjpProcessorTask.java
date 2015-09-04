41f41a8ae378e16726c396c58d75659fa0cf75bb
oleksiys <oleksiys@java.net>
2015-09-04 12:40:52 -0700

    [1.9.x] + fix a typo in shutdown secret check

--- modules/http-ajp/src/main/java/com/sun/grizzly/http/ajp/AjpProcessorTask.java
+++ modules/http-ajp/src/main/java/com/sun/grizzly/http/ajp/AjpProcessorTask.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2011-2012 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2011-2015 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -328,7 +328,7 @@ public class AjpProcessorTask extends ProcessorTask {
         final String secret = ajpConfiguration.getSecret();
         
         if (secret != null &&
-                secret.equals(shutdownSecret)) {
+                !secret.equals(shutdownSecret)) {
             throw new IllegalStateException("Secret doesn't match, no shutdown");
         }
 
