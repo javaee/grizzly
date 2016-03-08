d9ec247b1458b8bae26371fa456afe5e57a0e479
oleksiys <oleksiys@java.net>
2016-03-08 01:25:36 -0800

    [2.3.x] + https://java.net/jira/browse/GRIZZLY-1822 make skip more tolerant when input is closed, return 0 instead of throwing IOException

--- modules/http/src/main/java/org/glassfish/grizzly/http/io/InputBuffer.java
+++ modules/http/src/main/java/org/glassfish/grizzly/http/io/InputBuffer.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2010-2015 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2010-2016 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -700,10 +700,6 @@ public class InputBuffer {
         if (LOGGER.isLoggable(LOGGER_LEVEL)) {
             log("InputBuffer %s skip %s bytes. Ready content: %s",
                     this, n, inputContentBuffer);
-        }
-
-        if (closed) {
-            throw new IOException();
         }
 
         if (!processingChars) {
