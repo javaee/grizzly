1d9be9535ea24dd05ac5f04de3e69444bd54d3f4
Ryan Lubke <ryan.lubke@oracle.com>
2016-07-28 12:07:33 -0700

    [2.3.x] Document Http2Constants.

--- modules/http2/src/main/java/org/glassfish/grizzly/http2/Http2Constants.java
+++ modules/http2/src/main/java/org/glassfish/grizzly/http2/Http2Constants.java
@@ -41,11 +41,23 @@
 package org.glassfish.grizzly.http2;
 
 /**
- * Created by rlubke on 7/20/16.
+ * Constants for HTTP/2.
  */
 public interface Http2Constants {
 
-    static final String HTTP11 = "http/1.1";
-    static final String HTTP2 = "h2";
-    static final String HTTP2_CLEAR = "h2c";
+    /**
+     * ALPN token for HTTP/1.1.
+     */
+    String HTTP11 = "http/1.1";
+
+    /**
+     * ALPN token for HTTP/2.0.
+     */
+    String HTTP2 = "h2";
+
+    /**
+     * Token for clear-text HTTP/2.0.
+     */
+    String HTTP2_CLEAR = "h2c";
+
 }
