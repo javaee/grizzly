7192e6e4485eb32c2267989acc51b0b39c8d24b7
Ryan Lubke <ryan.lubke@oracle.com>
2016-08-17 10:28:47 -0700

    [2.3.x] Changes for https://java.net/jira/browse/GRIZZLY-1848.

--- modules/http2/src/main/java/org/glassfish/grizzly/http2/frames/ErrorCode.java
+++ modules/http2/src/main/java/org/glassfish/grizzly/http2/frames/ErrorCode.java
@@ -44,19 +44,20 @@ package org.glassfish.grizzly.http2.frames;
  * Error codes enum
  */
 public enum ErrorCode {
-    NO_ERROR(0),
-    PROTOCOL_ERROR(1),
-    INTERNAL_ERROR(2),
-    FLOW_CONTROL_ERROR(3),
-    SETTINGS_TIMEOUT(4),
-    STREAM_CLOSED(5),
-    FRAME_SIZE_ERROR(6),
-    REFUSED_STREAM(7),
-    CANCEL(8),
-    COMPRESSION_ERROR(9),
-    CONNECT_ERROR(10),
-    ENHANCE_YOUR_CALM(11),
-    INADEQUATE_SECURITY(12);
+    NO_ERROR(0x0),
+    PROTOCOL_ERROR(0x1),
+    INTERNAL_ERROR(0x2),
+    FLOW_CONTROL_ERROR(0x3),
+    SETTINGS_TIMEOUT(0x4),
+    STREAM_CLOSED(0x5),
+    FRAME_SIZE_ERROR(0x6),
+    REFUSED_STREAM(0x7),
+    CANCEL(0x8),
+    COMPRESSION_ERROR(0x9),
+    CONNECT_ERROR(0xa),
+    ENHANCE_YOUR_CALM(0xb),
+    INADEQUATE_SECURITY(0xc),
+    HTTP_1_1_REQUIRED(0xd);
     
     final int code;
     
